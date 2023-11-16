c0293451030b918fdea9657847edceeaa013057c
surinkwon <tnfls7070@gmail.com>
2023-11-16 17:17:40 +0900

    design [kanban-task-read] 컴포넌트 레이아웃에 디자인 추가
    sprint page, kanban board, column board, task card, progress bar에 디자인 코드 추가
    tailwind config에 foundation 색상 지정

--- FE/src/pages/sprint/SprintPage.tsx
+++ FE/src/pages/sprint/SprintPage.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import PrograssBar from '../../components/common/ProgressBar/ProgressBar';
 import KanbanBoard from '../../components/sprint/KanbanBoard';
+import BoardHeader from '../../components/sprint/BoardHeader';
 
 export interface Task {
   id: number;
@@ -8,7 +9,7 @@ export interface Task {
   user: string;
   point: number;
   state: 'ToDo' | 'InProgress' | 'Done';
-  storyName: string;
+  storyTitle: string;
 }
 
 interface Data {
@@ -23,7 +24,7 @@ const SprintPage = () => {
   const data: Data = {
     taskList: [
       {
-        storyName: '스토리1',
+        storyTitle: '유저는 로그인할 수 있다.',
         id: 2,
         title: '로그인 DB저장',
         user: '1',
@@ -31,7 +32,7 @@ const SprintPage = () => {
         state: 'ToDo',
       },
       {
-        storyName: '스토리2',
+        storyTitle: '유저는 에픽을 생성할 수 있다.',
         id: 3,
         title: 'API작성하기',
         user: '4',
@@ -39,7 +40,7 @@ const SprintPage = () => {
         state: 'ToDo',
       },
       {
-        storyName: '스토리1',
+        storyTitle: '유저는 로그인할 수 있다.',
         id: 4,
         title: '회원가입 기능 구현',
         user: '3',
@@ -47,7 +48,7 @@ const SprintPage = () => {
         state: 'InProgress',
       },
       {
-        storyName: '스토리3',
+        storyTitle: '유저는 칸반보드를 조회할 수 있다.',
         id: 5,
         title: 'UI 디자인',
         user: '4',
@@ -57,40 +58,88 @@ const SprintPage = () => {
     ],
   };
 
-  // storyName을 기준으로 데이터를 나누기
+  const todoList = data.taskList.filter(({ state }) => state === 'ToDo');
+  const inProgressList = data.taskList.filter(({ state }) => state === 'InProgress');
+  const doneList = data.taskList.filter(({ state }) => state === 'Done');
+
+  // storyTitle을 기준으로 데이터를 나누기
   const tasksByStory: TaskGroupedByStory = data.taskList.reduce((result: TaskGroupedByStory, current: Task) => {
-    const storyName = current.storyName;
-    result[storyName] = result[storyName] ?? [];
-    result[storyName].push(current);
+    const storyTitle = current.storyTitle;
+    result[storyTitle] = result[storyTitle] ?? [];
+    result[storyTitle].push(current);
     return result;
   }, {});
 
-  const storyKanbanBoards = Object.entries(tasksByStory).map(([storyName, taskList]) => (
-    <KanbanBoard key={storyName} storyName={storyName} taskList={taskList} />
-  ));
+  const storyKanbanBoards = Object.entries(tasksByStory).map(([storyTitle, taskList]) => {
+    const todoList = taskList.filter(({ state }) => state === 'ToDo');
+    const inProgressList = taskList.filter(({ state }) => state === 'InProgress');
+    const doneList = taskList.filter(({ state }) => state === 'Done');
+    return (
+      <li>
+        <KanbanBoard
+          key={storyTitle}
+          storyTitle={storyTitle}
+          storyId="LES-12"
+          todoList={todoList}
+          inProgressList={inProgressList}
+          doneList={doneList}
+        />
+      </li>
+    );
+  });
 
   const handleGroupButtonClick = () => {
     setTaskGroup((taskGroup) => (taskGroup === 'all' ? 'group' : 'all'));
   };
 
   return (
-    <>
-      <header>
-        <h1>스프린트 1</h1>
-        <span>백로그와 칸반보드가 보이는 이슈관리 프로토 타입을 만들자</span>
-      </header>
-      <div>
-        <div>
+    <section className="p-4">
+      <div className="flex mb-2.5">
+        <h1 className="mr-2 text-2xl font-bold text-starbucks-green">스프린트 1</h1>
+        <span className="self-end text-xs h-fit">백로그와 칸반보드가 보이는 이슈관리 프로토 타입을 만들자</span>
+      </div>
+      <div className="flex justify-between mb-4">
+        <div className="flex gap-x-8">
           <PrograssBar startText="23.11.14" endText="23.11.17" totalAmount={5} currentAmount={3} />
           <PrograssBar startText="태스크 60건" endText="완료 9건" totalAmount={60} currentAmount={9} />
         </div>
-        <div>
-          <button onClick={handleGroupButtonClick}>전체 태스크</button>
-          <button onClick={handleGroupButtonClick}>스토리 그룹화</button>
+        <div className="flex gap-2">
+          <button
+            className="bg-starbucks-green text-true-white rounded py-1.5 px-4 font-bold text-xs"
+            onClick={handleGroupButtonClick}
+          >
+            필터
+          </button>
+          <button
+            className="bg-starbucks-green text-true-white rounded py-1.5 px-4 font-bold text-xs"
+            onClick={handleGroupButtonClick}
+          >
+            스프린트 완료
+          </button>
         </div>
       </div>
-      <div>{taskGroup === 'all' ? <KanbanBoard taskList={data.taskList} /> : storyKanbanBoards}</div>
-    </>
+      <hr className="mb-2.5 bg-green-stroke border" />
+      <div>
+        <div className="flex justify-between">
+          <BoardHeader
+            boardName="할 일"
+            boardDescription="스프린트 내 아직 진행되지 않은 업무"
+            taskNumber={todoList.length}
+          />
+          <BoardHeader boardName="진행 중" boardDescription="현재 진행 중인 업무" taskNumber={inProgressList.length} />
+          <BoardHeader boardName="완료" boardDescription="스프린트 중 완료된 모든 업무" taskNumber={doneList.length} />
+        </div>
+        <ul className="flex flex-col gap-y-1.5">
+          {taskGroup === 'all' ? (
+            <li>
+              <KanbanBoard todoList={todoList} inProgressList={inProgressList} doneList={doneList} />
+            </li>
+          ) : (
+            storyKanbanBoards
+          )}
+        </ul>
+      </div>
+    </section>
   );
 };
 
