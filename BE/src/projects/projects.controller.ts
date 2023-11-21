581bfb789f7799e5af8c22356e563a6280bc189d
조영우 <joyeong-u@joyeong-uui-MacBookAir.local>
2023-11-21 11:30:47 +0900

    feat: [prject-create] 프로젝트 생성 API
    task: 프로젝트 데이터를 DB에 저장할 수 있다.
    1. project entity 프로젝트 디렉토리로 이동.
    2. createProject DTO정의하고 DB저장로직 작성

--- BE/src/projects/projects.controller.ts
+++ BE/src/projects/projects.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateProjectRequestDto, CreateProjectResponseDto } from './dto/Project.dto';
+import { ProjectsService } from './projects.service';
 
 @Controller('projects')
-export class ProjectsController {}
+export class ProjectsController {
+  constructor(private readonly projectsSevice: ProjectsService) {}
+  @Post()
+  createproject(@Body() body: CreateProjectRequestDto): Promise<CreateProjectResponseDto> {
+    return this.projectsSevice.createProject(body);
+  }
+}
--- BE/src/projects/projects.service.ts
+++ BE/src/projects/projects.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateProjectRequestDto, CreateProjectResponseDto } from './dto/Project.dto';
+import { Project } from './entity/project.entity';
 
 @Injectable()
-export class ProjectsService {}
+export class ProjectsService {
+  constructor(@InjectRepository(Project) private projectRespository: Repository<Project>) {}
+  async createProject(dto: CreateProjectRequestDto): Promise<CreateProjectResponseDto> {
+    const newProject = this.projectRespository.create({ name: dto.name });
+    const savedProject = await this.projectRespository.save(newProject);
+    return { id: savedProject.id };
+  }
+}
