877c2d993177407de265bac12561525bb2805bea
surinkwon <tnfls7070@gmail.com>
2023-11-22 16:00:05 +0900

    feat: [move-page] 사이드 네비게이션 바 기능 구현
    MainPage에 사이드 네비게이션 바 적용
    각 페이지로 이동할 수 있도록 함

--- FE/src/components/backlog/SideNavigationBar.tsx
+++ FE/src/components/backlog/SideNavigationBar.tsx
@@ -1,37 +1,33 @@
 import { NavigationInformation } from '../../types/navigation';
 import NavigationButton from './NavigationButton';
 import lesserLogo from '../../assets/icons/lesserLogo.svg';
+import { Link, useLocation } from 'react-router-dom';
 
 const navigationInformation: NavigationInformation[] = [
   {
     pageName: '백로그',
-    pageURI: 'backlog',
+    pageURI: '/backlog',
     description:
       '백로그 페이지 설명. 백로그는 제품백로그와 스프린트 백로그가 있으며 에픽, 스토리, 태스크로 이루어진다.',
   },
   {
     pageName: '스프린트',
-    pageURI: 'sprint',
+    pageURI: '/sprint',
     description:
       '스프린트 페이지 설명. 스프린트 시작하면 칸반보드를 확인할 수 있고, 드래그 앤 드롭을 이용해 티켓을 옮길 수 있다.',
   },
   {
     pageName: '회고',
-    pageURI: 'retrospect',
+    pageURI: '/review',
     description: '회고하는 페이지 설명. 스프린트 백로그 태스트 상태 분석, 번다운 차트, 회고 글 작성 칸이 있을 예정',
-  },
-  {
-    pageName: '내 프로젝트',
-    pageURI: 'projects',
-    description: '내 프로젝트를 볼 수 있는 페이지',
   },
 ];
 
 const SideNavigationBar = () => {
-  const currentURI = 'backlog';
+  const currentURI = useLocation().pathname;
 
   return (
-    <nav className="flex flex-col items-center px-6 py-8 rounded-lg w-52 bg-house-green gap-2.5">
+    <nav className="flex flex-col items-center px-6 py-8 rounded-lg w-52 bg-house-green gap-2.5 mr-10">
       <div className="mb-5">
         <img src={lesserLogo} alt="로고" />
         <p className="text-xs font-medium text-true-white">적고 쉽고 애자일하게 일하자</p>
@@ -44,7 +40,10 @@ const SideNavigationBar = () => {
           </li>
         ))}
       </ul>
-      <button className="py-3 text-lg font-bold text-true-white mb-44">로그아웃</button>
+      <Link to={'project'} className="py-3 text-lg font-bold text-true-white">
+        내 프로젝트
+      </Link>
+      <button className="py-3 mb-12 text-lg font-bold text-true-white">로그아웃</button>
     </nav>
   );
 };
