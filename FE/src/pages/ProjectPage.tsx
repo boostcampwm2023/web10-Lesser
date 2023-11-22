389fc73d3f8000a6ed3a7aed24c933e464d28e9f
dongind.oct <dongind.oct@gmail.com>
2023-11-22 17:03:40 +0900

    feat: [project-read] 프로젝트 목록을 리스트 형태로 출력
    1. project 개체애 대한 타입 지정
    2. ProjectCard 컴포넌트 정의
    3. ProjectCard를 호출하여 dummyData 내 프로젝트 데이터를 렌더링

--- FE/src/pages/ProjectPage.tsx
+++ FE/src/pages/ProjectPage.tsx
@@ -1,5 +1,46 @@
+interface projectElement {
+  title: string;
+  description: string;
+  taskNum: number;
+}
+
+const ProjectCard = ({ title, description, taskNum }: projectElement) => {
+  return (
+    <div className="w-[11.25rem] h-[7.5rem] bg-true-white px-3 pt-3 pb-6">
+      <div>{title}</div>
+      <div>{description}</div>
+      <div>내 진행 업무</div>
+      <div>{taskNum}</div>
+    </div>
+  );
+};
+
 const ProjectPage = () => {
-  return <div>ProjectPage</div>;
+  const dummyData: Array<projectElement> = [
+    {
+      title: 'Lesser',
+      description: '애자일 프로젝트 관리 툴',
+      taskNum: 3,
+    },
+    {
+      title: 'Lesser',
+      description: '애자일 프로젝트 관리 툴',
+      taskNum: 3,
+    },
+    {
+      title: 'Lesser',
+      description: '애자일 프로젝트 관리 툴',
+      taskNum: 3,
+    },
+  ];
+
+  return (
+    <div>
+      {dummyData.map(({ title, description, taskNum }) => (
+        <ProjectCard title={title} description={description} taskNum={taskNum} />
+      ))}
+    </div>
+  );
 };
 
 export default ProjectPage;
