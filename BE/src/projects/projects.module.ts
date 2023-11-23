a393023a95ce14ec7f58414c979d23b73efaf97e
조영우 <joyeong-u@joyeong-uui-MacBookAir.local>
2023-11-23 17:54:07 +0900

    feat: [project-read] 프로젝트 태스크개수 추가구현
    1. 기존프로젝트 조회의 응답에  프로젝트 태스크개수 추가
    2. 빠른구현을 위한 비효율적인 로직을 작성함. 추후 수정예정

--- BE/src/projects/projects.module.ts
+++ BE/src/projects/projects.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Epic } from 'src/backlogs/entities/epic.entity';
+import { Story } from 'src/backlogs/entities/story.entity';
+import { Task } from 'src/backlogs/entities/task.entity';
 import { Project } from './entity/project.entity';
 import { ProjectsController } from './projects.controller';
 import { ProjectsService } from './projects.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Project])],
+  imports: [TypeOrmModule.forFeature([Project, Epic, Story, Task])],
   controllers: [ProjectsController],
   providers: [ProjectsService],
 })
--- BE/src/projects/projects.service.ts
+++ BE/src/projects/projects.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Epic } from 'src/backlogs/entities/epic.entity';
+import { Story } from 'src/backlogs/entities/story.entity';
+import { Task } from 'src/backlogs/entities/task.entity';
 import { Repository } from 'typeorm';
 import { CreateProjectRequestDto, CreateProjectResponseDto, ReadProjectListResponseDto } from './dto/Project.dto';
 import { Project } from './entity/project.entity';
 
 @Injectable()
 export class ProjectsService {
-  constructor(@InjectRepository(Project) private projectRespository: Repository<Project>) {}
+  constructor(
+    @InjectRepository(Project) private projectRespository: Repository<Project>,
+    @InjectRepository(Epic) private epicRepository: Repository<Epic>,
+    @InjectRepository(Story) private storyRepository: Repository<Story>,
+    @InjectRepository(Task) private taskRepository: Repository<Task>,
+  ) {}
 
   async createProject(dto: CreateProjectRequestDto): Promise<CreateProjectResponseDto> {
     const newProject = this.projectRespository.create({ name: dto.name, subject: dto.subject });
@@ -16,14 +24,29 @@ export class ProjectsService {
 
   async readProjectList(): Promise<ReadProjectListResponseDto[]> {
     const projectListData = await this.projectRespository.find();
-    const projectList = projectListData.map((projectData) => {
-      const project = new ReadProjectListResponseDto();
-      project.id = projectData.id;
-      project.name = projectData.name;
-      project.subject = projectData.subject;
-      project.nextPage = 'backlogs';
-      return project;
-    });
+    const projectList = Promise.all(
+      projectListData.map(async (projectData) => {
+        const project = new ReadProjectListResponseDto();
+        project.id = projectData.id;
+        project.name = projectData.name;
+        project.subject = projectData.subject;
+        project.nextPage = 'backlogs';
+        project.myTaskCount = await this.getTaskCount(project.id);
+        return project;
+      }),
+    );
     return projectList;
+  }
+
+  private async getTaskCount(projectId: number): Promise<number> {
+    let ret = 0;
+    const epicDataList = await this.epicRepository.find({ where: { project: { id: projectId } } });
+    for (let i = 0; i < epicDataList.length; i++) {
+      const storyDataList = await this.storyRepository.find({ where: { epic: { id: epicDataList[i].id } } });
+      for (let j = 0; j < storyDataList.length; j++) {
+        ret += (await this.taskRepository.find({ where: { story: { id: storyDataList[j].id } } })).length;
+      }
+    }
+    return ret;
   }
 }
