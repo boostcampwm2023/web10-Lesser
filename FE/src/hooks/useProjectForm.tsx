d451052673d15f0432a82ea551a5c714b724e963
dongind.oct <dongind.oct@gmail.com>
2023-11-23 16:33:21 +0900

    feat: [project-create] project POST api 연결
    1. project api 연결
    2. local 서버에서 post test 완료

--- FE/src/hooks/useProjectForm.tsx
+++ FE/src/hooks/useProjectForm.tsx
@@ -1,24 +1,45 @@
 import { useState, useRef } from 'react';
+import { api } from '../apis/api';
+import { NavigateFunction, useNavigate } from 'react-router-dom';
+
+interface projectPostBody {
+  name: string;
+  subject: string;
+}
+
+const fetchProjectPost = ({ name, subject }: projectPostBody, navigate: NavigateFunction) => {
+  api
+    .post('projects', {
+      name,
+      subject,
+    })
+    .then((response) => {
+      const projectId = response.data.id;
+      // 임시 backlog 연결
+      navigate('/backlog');
+    });
+};
 
 const useProjectForm = () => {
   const [process, setProcess] = useState<number>(0);
   const [projectName, setProjectName] = useState<string>('');
   const projectNameRef = useRef<HTMLInputElement>(null);
   const projectSubjectRef = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
   const handleNextButtonClick = () => {
     if (!projectNameRef.current) {
       return;
     }
     setProjectName(projectNameRef.current.value);
-    setProcess((process) => (!process ? process + 1 : 0));
+    setProcess((process) => process + 1);
     projectNameRef.current.value = '';
   };
   const handleSubmitButtonClick = () => {
     if (!projectSubjectRef.current) {
       return;
     }
-    // post 코드 작성 예정
-    console.log(projectName, projectSubjectRef.current.value);
+    const projectSubject = projectSubjectRef.current.value;
+    fetchProjectPost({ name: projectName, subject: projectSubject }, navigate);
   };
 
   return {
