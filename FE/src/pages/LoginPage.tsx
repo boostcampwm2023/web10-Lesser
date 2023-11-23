e56dcc32e71a406b1913ae76713ae03ada2f6b2c
surinkwon <tnfls7070@gmail.com>
2023-11-23 13:54:33 +0900

    feat: [user-login] OAuth를 통해 코드를 받아오는 로직 작성
    OAuth에서 login 페이지로 리다이렉트될 때 query parameter에 있는 code를 받아오는 로직 작성
    code가 있으면 배경을 가리는 효과 추가
    이후 api 요청을 통해 accessToken을 가져오는 로직 작성 예정

--- FE/src/pages/LoginPage.tsx
+++ FE/src/pages/LoginPage.tsx
@@ -1,13 +1,14 @@
-import { useNavigate } from 'react-router-dom';
 import GithubLogin from '../assets/icons/GithubLogin';
 import LesserTextLogo from '../assets/icons/LesserTextLogo';
 import loginImage from '../assets/images/login-image.png';
+import { useSearchParams } from 'react-router-dom';
 
 const LoginPage = () => {
-  const navigate = useNavigate();
+  const [serchParams] = useSearchParams();
+  const code = serchParams.get('code');
 
   return (
-    <div className="flex h-full">
+    <div className="flex h-full min-w-[95rem]">
       <div className="flex items-center justify-center w-1/2">
         <img className="w-10/12 h-10/12" src={loginImage} alt="로그인하는 레서" />
       </div>
@@ -15,10 +16,11 @@ const LoginPage = () => {
         <LesserTextLogo width={256} height={56} color="#006241" />
         <p className="mt-5 mb-16 text-2xl font-medium">적고 쉽고 애자일하게 일하자</p>
         <p className="mt-2 mb-3 text-base font-bold">GitHub 로그인</p>
-        <button onClick={() => navigate('/backlog')}>
+        <a href={code ? undefined : `${import.meta.env.VITE_OAUTH_URL}`}>
           <GithubLogin />
-        </button>
+        </a>
       </div>
+      {code && <div className="absolute w-full h-full bg-white/[0.4] "></div>}
     </div>
   );
 };
