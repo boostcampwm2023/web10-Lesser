6ac9af4c0a9b69317d1bbb8efd4798de2d5dd3c0
조영우 <joyeong-u@joyeong-uui-MacBookAir.local>
2023-11-23 13:12:35 +0900

    fix: [cors-config] 배포를 위한 CORS임시 설정
    1. CORS정책을 전체허용으로 임시설정함

--- BE/src/main.ts
+++ BE/src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('Lesser API')
