d01b0d3a7395f27316daff036433ee0f5577def7
조영우 <joyeong-u@joyeong-uui-MacBookAir.local>
2023-11-26 20:08:26 +0900

    feat: [project-create] 유저, 프로젝트 다대다 테이블 생성
    1. project 엔티티에 유저와 조인하는 테이블 생성
    2. isLoginGuard에서 request에 유저정보 추가하는 로직을 객체형태로 추가하도록 수정
    3. 유저정보를 파라미터 데코레이터 형태로 뽑도록 커스텀 파라미터 데코레이터와 타입 정의
    4. createProject에서 프로젝트 생성뿐만 아니라 프로젝트-유저 테이블에 유저정보를 함께 추가하는 로직 추가

---
+++ BE/src/common/types/memberDecorator.type.ts
@@ -0,0 +1,3 @@
+export class memberDecoratorType {
+  id: number;
+}
--- BE/src/projects/projects.controller.ts
+++ BE/src/projects/projects.controller.ts
@@ -1,15 +1,20 @@
 import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { IsLoginGuard } from 'src/common/auth/IsLogin.guard';
+import { Member } from 'src/common/decorators/memberDecorator';
+import { memberDecoratorType } from 'src/common/types/memberDecorator.type';
 import { CreateProjectRequestDto, CreateProjectResponseDto, ReadProjectListResponseDto } from './dto/Project.dto';
 import { ProjectsService } from './projects.service';
 
-// @UseGuards(IsLoginGuard)
+@UseGuards(IsLoginGuard)
 @Controller('projects')
 export class ProjectsController {
   constructor(private readonly projectsSevice: ProjectsService) {}
   @Post()
-  createproject(@Body() body: CreateProjectRequestDto): Promise<CreateProjectResponseDto> {
-    return this.projectsSevice.createProject(body);
+  createproject(
+    @Member() memberInfo: memberDecoratorType,
+    @Body() body: CreateProjectRequestDto,
+  ): Promise<CreateProjectResponseDto> {
+    return this.projectsSevice.createProject(body, memberInfo);
   }
 
   @Get()
