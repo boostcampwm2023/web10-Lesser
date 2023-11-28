e1be0c781bccd0213452f328e5a7e6be645c0d03
Suhjeong Kim <kimsj.py@gmail.com>
2023-11-28 21:54:08 +0900

    feat: 스프린트 생성 API, 회고 생성 API
    1. 스프린트 Entity 작성: 프로젝트와 다대일 매핑
    2. 회고 Entity 작성: 스프린트와 일대일 매핑
    3. 자동으로 기입되는 생성일과 수정일 추가
    3. 스프린트 생성 API, 회고 생성 API 작성

--- BE/src/reviews/reviews.service.ts
+++ BE/src/reviews/reviews.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Review } from './entities/review.entity';
+import { Repository } from 'typeorm';
+import { Sprint } from 'src/sprints/entities/sprint.entity';
+import { CreateReviewRequestDto, CreateReviewResponseDto } from './dto/Review.dto';
 
 @Injectable()
-export class ReviewsService {}
+export class ReviewsService {
+  constructor(
+    @InjectRepository(Review) private reviewRepository: Repository<Review>,
+    @InjectRepository(Sprint) private sprintRepository: Repository<Sprint>,
+  ) {}
+
+  async createReview(
+    dto: CreateReviewRequestDto,
+    // memberInfo: memberDecoratorType,
+  ): Promise<CreateReviewResponseDto> {
+    // const member = await this.memberRepository.findOne({ where: { id: memberInfo.id } });
+    // if (!member) throw new InternalServerErrorException();
+    const sprint = await this.sprintRepository.findOne({ where: { id: dto.sprintId } });
+    const newReview = this.reviewRepository.create({
+      content: dto.content,
+      sprint: sprint,
+    });
+    const savedReview = await this.sprintRepository.save(newReview);
+    return { id: savedReview.id };
+  }
+}
--- BE/src/sprints/sprints.service.ts
+++ BE/src/sprints/sprints.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Sprint } from './entities/sprint.entity';
+import { Repository } from 'typeorm';
+import { Project } from 'src/projects/entity/project.entity';
+import { CreateSprintRequestDto, CreateSprintResponseDto } from './dto/Sprint.dto';
+import { memberDecoratorType } from 'src/common/types/memberDecorator.type';
+import { Review } from 'src/reviews/entities/review.entity';
 
 @Injectable()
-export class SprintsService {}
+export class SprintsService {
+  constructor(
+    @InjectRepository(Sprint) private sprintRepository: Repository<Sprint>,
+    @InjectRepository(Project) private projectRepository: Repository<Project>,
+    @InjectRepository(Review) private reviewRepository: Repository<Review>,
+  ) {}
+
+  async createSprint(
+    dto: CreateSprintRequestDto,
+    // memberInfo: memberDecoratorType,
+  ): Promise<CreateSprintResponseDto> {
+    // const member = await this.memberRepository.findOne({ where: { id: memberInfo.id } });
+    // if (!member) throw new InternalServerErrorException();
+    const project = await this.projectRepository.findOne({ where: { id: dto.projectId } });
+    const newSprint = this.sprintRepository.create({
+      title: dto.title,
+      goal: dto.goal,
+      start_date: dto.startDate,
+      end_date: dto.endDate,
+      project: project,
+    });
+    const savedSprint = await this.sprintRepository.save(newSprint);
+    return { id: savedSprint.id };
+  }
+}
