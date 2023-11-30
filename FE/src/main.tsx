d38661496a7f1abb0d28bc27f7e04f4b6baca496
whiteseungmin <tcsml@naver.com>
2023-11-30 21:58:42 +0900

    feat: [review-read] @tanstack/react-query를 이용한 데이터 관리

--- FE/src/main.tsx
+++ FE/src/main.tsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
