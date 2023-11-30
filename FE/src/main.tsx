d42b988246277c6adff3644b6e986c8d9788a015
surinkwon <tnfls7070@gmail.com>
2023-11-30 17:16:57 +0900

    chore: [kanban-drag] strict mode 재설정, react-beautiful-dnd 재설치

--- FE/src/main.tsx
+++ FE/src/main.tsx
@@ -5,9 +5,9 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  // </React.StrictMode>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 );
