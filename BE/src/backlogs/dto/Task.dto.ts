53c8cb4ef9e7faaa488fbc027a902450e31de3e2
조영우 <joyeong-u@joyeong-uui-MacBookAir.local>
2023-11-30 21:18:46 +0900

    fix: [backlog-create] 태스크 생성 state 디폴트값으로 변경

--- BE/src/backlogs/dto/Task.dto.ts
+++ BE/src/backlogs/dto/Task.dto.ts
@@ -1,7 +1,7 @@
 import { IntersectionType, OmitType, PartialType, PickType } from '@nestjs/swagger';
 import { baseTaskDto } from './baseType.dto';
 
-export class CreateBacklogsTaskRequestDto extends OmitType(baseTaskDto, ['id', 'sequence']) {}
+export class CreateBacklogsTaskRequestDto extends OmitType(baseTaskDto, ['id', 'sequence', 'state']) {}
 export class DeleteBacklogsTaskRequestDto extends PickType(baseTaskDto, ['id']) {}
 export class UpdateBacklogsRequestTaskDto extends IntersectionType(
   PickType(baseTaskDto, ['id']),
