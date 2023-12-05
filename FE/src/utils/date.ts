fd332dee917b56fc83d7366013531f49ab11b01c
whiteseungmin <tcsml@naver.com>
2023-12-05 21:35:50 +0900

    fix: transformDate 함수의 날짜 일자 계산 오류 수정

--- FE/src/utils/date.ts
+++ FE/src/utils/date.ts
@@ -2,9 +2,8 @@ const transformDate = (date: string) => {
   const newDate = new Date(date);
   const year = newDate.getFullYear();
   const month = (newDate.getMonth() + 1).toString().padStart(2, '0');
-  const day = newDate.getDay().toString().padStart(2, '0');
+  const day = newDate.getDate().toString().padStart(2, '0');
 
   return `${year}-${month}-${day}`;
 };
-
 export { transformDate };
