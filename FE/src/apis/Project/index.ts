c61ac7b793331fc7ee7cb0b2995011e3cc8df6f4
dongind.oct <dongind.oct@gmail.com>
2023-12-07 11:35:40 +0900

    fix: api 호출 에러 수정
    - Jenkins 빌드 중 project api를 호출하지 못하는 문제가 발생하여, index 호출과 관련되 에러 여부를 확인하기 위해 index 파일을 삭제하고 직접 각 api 함수를 호출 하도록 코드를 작성

--- FE/src/apis/Project/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as fetchGetMyProjects } from './fetchGetMyProjects.ts';
-export { default as fetchGetUsername } from './fetchGetUsername.ts';
-export { default as fetchPostProject } from './fetchPostProject.ts';
--- FE/src/hooks/queries/project/useGetMyProjects.ts
+++ FE/src/hooks/queries/project/useGetMyProjects.ts
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import { fetchGetMyProjects } from '../../../apis/project';
+import fetchGetMyProjects from '../../../apis/project/fetchGetMyProjects';
 
 const useGetMyProjects = () =>
   useQuery({
