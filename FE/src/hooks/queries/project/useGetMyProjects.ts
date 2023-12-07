a7106dd0805fdb6f2002389c0a97dcc021c3b64a
dongind <109324473+dongind@users.noreply.github.com>
2023-12-07 11:46:03 +0900

    Revert "fix: api 호출 에러 수정"

--- FE/src/hooks/queries/project/useGetMyProjects.ts
+++ FE/src/hooks/queries/project/useGetMyProjects.ts
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import fetchGetMyProjects from '../../../apis/project/fetchGetMyProjects';
+import { fetchGetMyProjects } from '../../../apis/project';
 
 const useGetMyProjects = () =>
   useQuery({
