0a3ac916793cfc1c884436e4c235e7362279cb20
whiteseungmin <tcsml@naver.com>
2023-12-12 21:30:18 +0900

    feat: footer 영역 구현

--- FE/src/constants/constants.tsx
+++ FE/src/constants/constants.tsx
@@ -1,3 +1,6 @@
+import GithubIcon from '../assets/icons/GithubIcon';
+import NotionIcon from '../assets/icons/NotionIcon';
+
 export const BASEURL = 'https://lesser-project.site/api';
 
 export const REFRESH_TOKEN = 'refreshToken';
@@ -27,3 +30,16 @@ export const BACKLOG_URL = {
 };
 
 export const SPRINT_BACKLOG_DROP_ID = 'sprintBacklog';
+
+export const contributors = [
+  { name: '권수린', link: 'https://github.com/surinkwon' },
+  { name: '김서정', link: 'https://github.com/kimsj-git' },
+  { name: '김용현', link: 'https://github.com/dongind' },
+  { name: '백승민', link: 'https://github.com/whiteseungmin' },
+  { name: '조영우', link: 'https://github.com/choyoungwoo9' },
+];
+
+export const socialLinks = [
+  { icon: <GithubIcon />, link: 'https://github.com/boostcampwm2023/web10-Lesser' },
+  { icon: <NotionIcon />, link: 'https://plastic-toad-cb0.notion.site/Lesser-b7bc965c2b154a0aafb1bf1e94c48cdc' },
+];
