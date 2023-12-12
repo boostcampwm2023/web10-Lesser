4d909f82b57b97f374f83cc7dc2536a515603191
dongind.oct <dongind.oct@gmail.com>
2023-12-12 10:36:50 +0900

    test 코드 예시 수정

--- FE/src/test/utils.test.ts
+++ FE/src/test/utils.test.ts
@@ -6,8 +6,17 @@ const sampleTaskList1: Array<ReadBacklogTaskResponseDto> = [
     id: 2,
     title: '[BE]에픽 목록을 화면이 렌더링',
     sequence: 1,
-    point: 3,
+    point: 1,
     userId: null,
+    condition: '인수조건 인수분해 인수합병 인수인계',
+    state: 'Todo',
+  },
+  {
+    id: 3,
+    sequence: 2,
+    title: '[BE]에픽 목록을 화면이 렌더링',
+    point: 2,
+    userId: 1,
     condition: '인수조건 인수분해 인수합병 인수인계',
     state: 'Todo',
   },
@@ -27,7 +36,7 @@ const sampleTaskList2: Array<ReadBacklogTaskResponseDto> = [
     id: 2,
     title: '[BE]에픽 목록을 화면이 렌더링',
     sequence: 1,
-    point: 3,
+    point: 1,
     userId: null,
     condition: '인수조건 인수분해 인수합병 인수인계',
     state: 'Todo',
@@ -36,10 +45,19 @@ const sampleTaskList2: Array<ReadBacklogTaskResponseDto> = [
     id: 3,
     sequence: 2,
     title: '[BE]에픽 목록을 화면이 렌더링',
-    point: 3,
+    point: 2,
     userId: 1,
     condition: '인수조건 인수분해 인수합병 인수인계',
     state: 'Done',
+  },
+  {
+    id: 3,
+    sequence: 2,
+    title: '[BE]에픽 목록을 화면이 렌더링',
+    point: 3,
+    userId: 1,
+    condition: '인수조건 인수분해 인수합병 인수인계',
+    state: 'Todo',
   },
 ];
 const sampleTaskList3: Array<ReadBacklogTaskResponseDto> = [
@@ -47,7 +65,7 @@ const sampleTaskList3: Array<ReadBacklogTaskResponseDto> = [
     id: 2,
     title: '[BE]에픽 목록을 화면이 렌더링',
     sequence: 1,
-    point: 3,
+    point: 1,
     userId: null,
     condition: '인수조건 인수분해 인수합병 인수인계',
     state: 'Todo',
@@ -56,10 +74,19 @@ const sampleTaskList3: Array<ReadBacklogTaskResponseDto> = [
     id: 3,
     sequence: 2,
     title: '[BE]에픽 목록을 화면이 렌더링',
-    point: 3,
+    point: 2,
     userId: 1,
     condition: '인수조건 인수분해 인수합병 인수인계',
     state: 'InProgress',
+  },
+  {
+    id: 3,
+    sequence: 2,
+    title: '[BE]에픽 목록을 화면이 렌더링',
+    point: 3,
+    userId: 1,
+    condition: '인수조건 인수분해 인수합병 인수인계',
+    state: 'Todo',
   },
 ];
 
@@ -67,7 +94,7 @@ test('스토리 포인트 계산1', () => {
   expect(getStoryPoint(sampleTaskList1)).toEqual({ TOTAL: 6, REMAIN: 6 });
 });
 test('스토리 포인트 계산2', () => {
-  expect(getStoryPoint(sampleTaskList2)).toEqual({ TOTAL: 6, REMAIN: 3 });
+  expect(getStoryPoint(sampleTaskList2)).toEqual({ TOTAL: 6, REMAIN: 4 });
 });
 test('스토리 포인트 계산3', () => {
   expect(getStoryPoint(sampleTaskList3)).toEqual({ TOTAL: 6, REMAIN: 6 });
