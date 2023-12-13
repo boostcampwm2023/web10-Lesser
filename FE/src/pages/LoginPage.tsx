5aa10dbfa5801f09c2b86537ac032a6045379870
surinkwon <tnfls7070@gmail.com>
2023-12-13 17:30:41 +0900

    feat: [loading] 로딩 컴포넌트 생성
    레서판다 이미지를 이용해 로그인 후 로딩 되는 시간 동안 보여줄 컴포넌트 구현

--- FE/src/pages/LoginPage.tsx
+++ FE/src/pages/LoginPage.tsx
@@ -1,6 +1,7 @@
 import GithubLogin from '../assets/icons/GithubLogin';
 import LesserTextLogo from '../assets/icons/LesserTextLogo';
 import loginImage from '../assets/images/login-image.png';
+import Loading from '../components/common/Loading';
 import useLogin from '../hooks/pages/useLogin';
 
 const LoginPage = () => {
@@ -8,18 +9,23 @@ const LoginPage = () => {
 
   return (
     <div className="flex h-screen min-w-[95rem] w-screen">
-      <div className="flex items-center justify-center w-1/2 bg-true-white">
-        <img className="w-[38rem] h-[38rem]" src={loginImage} alt="로그인하는 레서" />
-      </div>
-      <div className="flex flex-col items-center justify-center w-1/2 bg-cool-neutral">
-        <LesserTextLogo width={272} height={72} color="#006241" />
-        <p className="mt-5 mb-16 text-2xl font-medium">적고 쉽고 애자일하게 일하자</p>
-        <p className="mt-2 mb-3 text-base font-bold">GitHub 로그인</p>
-        <button onClick={handleLoginButtonClick} disabled={code ? true : false}>
-          <GithubLogin />
-        </button>
-      </div>
-      {code && <div className="absolute w-screen h-screen bg-white/[0.4]"></div>}
+      {code ? (
+        <Loading />
+      ) : (
+        <>
+          <div className="flex items-center justify-center w-1/2 bg-true-white">
+            <img className="w-[38rem] h-[38rem]" src={loginImage} alt="로그인하는 레서" />
+          </div>
+          <div className="flex flex-col items-center justify-center w-1/2 bg-cool-neutral">
+            <LesserTextLogo width={272} height={72} color="#006241" />
+            <p className="mt-5 mb-16 text-2xl font-medium">적고 쉽고 애자일하게 일하자</p>
+            <p className="mt-2 mb-3 text-base font-bold">GitHub 로그인</p>
+            <button onClick={handleLoginButtonClick} disabled={code ? true : false}>
+              <GithubLogin />
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
