d9cb685d7c28d409da88caa7e809b40edcd029b4
surinkwon <tnfls7070@gmail.com>
2023-12-14 11:52:53 +0900

    design: [user-login] 로딩 컴포넌트 가운데 정렬, 로그인 페이지 크기 조정
    로딩 컴포넌트가 가로 가운데 정렬 되지 않는 문제 수정
    로그인 페이지 크기 조정으로 페이지에 맞춰질 수 있도록 수정

--- FE/src/components/common/Loading.tsx
+++ FE/src/components/common/Loading.tsx
@@ -1,7 +1,7 @@
 import LoadingLesser from '../../assets/images/loading-image.png';
 
 const Loading = () => (
-  <div className="flex items-center justify-center h-full">
+  <div className="flex items-center justify-center w-full h-full">
     <img className="w-[7%] animate-[bounce_0.9s_ease-in-out_infinite]" src={LoadingLesser} alt="레서 판다" />
     <img className="w-[7%] animate-[bounce_0.9s_ease-in-out_0.1s_infinite]" src={LoadingLesser} alt="레서 판다" />
     <img className="w-[7%] animate-[bounce_0.9s_ease-in-out_0.3s_infinite]" src={LoadingLesser} alt="레서 판다" />
