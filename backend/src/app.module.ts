8baa838b73193dc7076f7520b0c448e5e57a35f6
조영우 <joyeong-u@joyeong-uui-MacBookAir.local>
2024-01-31 16:08:35 +0900

    feat: 백엔드 MySQL 연동

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (ConfigService: ConfigService) => ({
+        type: 'mysql',
+		host: ConfigService.get('DATABASE_HOST'),
+        port: +ConfigService.get('DATABASE_PORT'),
+        username: ConfigService.get('DATABASE_USER'),
+        password: ConfigService.get('DATABASE_PASSWORD'),
+        database: ConfigService.get('DATABASE_NAME'),
+        entities: [],
+        synchronize: true,
+      }),
+    }),
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+	  isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
