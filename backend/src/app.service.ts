29d908777ed959be3943c15f7969107ac1804425
조영우 <joyeong-u@joyeong-uui-MacBookAir.local>
2024-02-01 14:43:24 +0900

    feat: 백엔드 테스트3

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello BackEnd!222';
+    return 'Hello BackEnd!333';
   }
 }
