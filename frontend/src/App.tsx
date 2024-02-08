11e713569d3d0c219f0ecc1eec171f63ccc02549
조영우 <joyeong-u@joyeong-uui-MacBookAir.local>
2024-02-08 15:07:07 +0900

    feat: 테스트 문자열 변경

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React 2</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
