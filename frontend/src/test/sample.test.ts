201a06c145060dbb54785fa0eb1f6a062995931f
dongind.oct <dongind.oct@gmail.com>
2024-02-19 20:27:27 +0900

    MSW : Jest react - typescript 설정
    1. Jest를 react 및 typescript에서 사용할 수 있도록 세팅

--- frontend/src/test/sample.test.ts
+++ frontend/src/test/sample.test.ts
@@ -1,5 +1,11 @@
+import { sampleFunction } from "../utils/sampleFunction";
+
 describe("sample Test", () => {
   it("number test", () => {
     expect(3 + 4).toBe(7);
+  });
+
+  it("import test", () => {
+    expect(sampleFunction(3, 4)).toBe(7);
   });
 });
