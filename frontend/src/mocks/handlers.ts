5eec9cb23bfc805df2d04ec7323b5fd5ddc75b79
dongind.oct <dongind.oct@gmail.com>
2024-02-19 19:13:09 +0900

    MSW : browser integration
    main.tsx에서 조건부로 mocking을 수행하도록 코드를 작성
    1. enbaleMocking 함수 작성
    - 주의 사항: onUnhandledRequest의 설정값을 "bypass"로 설정하여, 모킹하지 않는 요청에 대한 경고 콘솔을 제거
    2. App.tsx에서 "/test/"를 통해 요청하는 코드를 작성
    - 정상적으로 모킹 데이터를 전달 받는 것을 확인

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -1,7 +1,7 @@
 import { http, HttpResponse } from "msw";
 
 export const handlers = [
-  http.get("/test", () => {
+  http.get("http://localhost:5173/test", () => {
     return HttpResponse.json(["This", "is", "test"]);
   }),
 ];
