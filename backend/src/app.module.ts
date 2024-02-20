f8bfc03381bbb0911c3072c5c06aa7ae53139614
Suhjeong Kim <kimsj.py@gmail.com>
2024-02-20 16:31:24 +0900

    feat: 환경변수 상수화, 환경변수 읽어오는 LesserConfigModule 테스트코드 작성
    Co-Authored-By: 조영우 <66576231+choyoungwoo9@users.noreply.github.com>

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,6 +6,14 @@ import { ConfigService } from '@nestjs/config';
 import { Member } from './member.entity';
 import { AuthModule } from './auth/auth.module';
 import { LesserConfigModule } from './lesser-config/lesser-config.module';
+import { GithubApiModule } from './github-api/github-api.module';
+import {
+  DATABASE_HOST,
+  DATABASE_PORT,
+  DATABASE_USER,
+  DATABASE_PASSWORD,
+  DATABASE_NAME,
+} from './lesser-config/constants';
 
 @Module({
   imports: [
@@ -13,11 +21,11 @@ import { LesserConfigModule } from './lesser-config/lesser-config.module';
       inject: [ConfigService],
       useFactory: (ConfigService: ConfigService) => ({
         type: 'mysql',
-        host: ConfigService.get('DATABASE_HOST'),
-        port: +ConfigService.get('DATABASE_PORT'),
-        username: ConfigService.get('DATABASE_USER'),
-        password: ConfigService.get('DATABASE_PASSWORD'),
-        database: ConfigService.get('DATABASE_NAME'),
+        host: ConfigService.get(DATABASE_HOST),
+        port: +ConfigService.get(DATABASE_PORT),
+        username: ConfigService.get(DATABASE_USER),
+        password: ConfigService.get(DATABASE_PASSWORD),
+        database: ConfigService.get(DATABASE_NAME),
         entities: [Member],
         synchronize: ConfigService.get('NODE_ENV') == 'PROD' ? false : true,
       }),
@@ -25,6 +33,7 @@ import { LesserConfigModule } from './lesser-config/lesser-config.module';
     TypeOrmModule.forFeature([Member]),
     AuthModule,
     LesserConfigModule,
+    GithubApiModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ backend/src/lesser-config/lesser-config.module.spec.ts
@@ -0,0 +1,36 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigService } from '@nestjs/config';
+import { LesserConfigModule } from './lesser-config.module';
+import {
+  GITHUB_CLIENT_ID,
+  GITHUB_CLIENT_SECRETS,
+  DATABASE_HOST,
+  DATABASE_PORT,
+  DATABASE_USER,
+  DATABASE_PASSWORD,
+  DATABASE_NAME,
+} from './constants';
+
+describe('Lesser Config Unit Test', () => {
+  let configService: ConfigService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [LesserConfigModule],
+    }).compile();
+
+    configService = module.get<ConfigService>(ConfigService);
+  });
+
+  describe('Get Env Variables', () => {
+    it('should return env variables', async () => {
+      expect(configService.get(GITHUB_CLIENT_ID)).toBeDefined();
+      expect(configService.get(GITHUB_CLIENT_SECRETS)).toBeDefined();
+      expect(configService.get(DATABASE_HOST)).toBeDefined();
+      expect(configService.get(DATABASE_PORT)).toBeDefined();
+      expect(configService.get(DATABASE_USER)).toBeDefined();
+      expect(configService.get(DATABASE_PASSWORD)).toBeDefined();
+      expect(configService.get(DATABASE_NAME)).toBeDefined();
+    });
+  });
+});
