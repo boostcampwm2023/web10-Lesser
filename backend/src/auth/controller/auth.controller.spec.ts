91aafe8c5800e8b7be1bb65fe5d5f47c7d1df121
Suhjeong Kim <kimsj.py@gmail.com>
2024-02-20 16:43:13 +0900

    feat: 깃허브 access token을 반환하는 서비스 로직에서 에러 발생시 컨트롤러가 401 에러를 던지도록 구현
    Co-Authored-By: 조영우 <66576231+choyoungwoo9@users.noreply.github.com>

--- backend/src/auth/controller/auth.controller.spec.ts
+++ backend/src/auth/controller/auth.controller.spec.ts
@@ -1,8 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from '../service/auth.service';
+import { UnauthorizedException } from '@nestjs/common';
 
-describe('Controller getGithubAuthServerUrl Unit Test', () => {
+describe('Auth Controller Unit Test', () => {
   let controller: AuthController;
   let authService: AuthService;
 
@@ -13,7 +14,8 @@ describe('Controller getGithubAuthServerUrl Unit Test', () => {
         {
           provide: AuthService,
           useValue: {
-            getGithubAuthUrl: jest.fn().mockResolvedValue('github auth url'),
+            getGithubAuthUrl: jest.fn(),
+            getAccessToken: jest.fn(),
           },
         },
       ],
@@ -23,10 +25,33 @@ describe('Controller getGithubAuthServerUrl Unit Test', () => {
     authService = module.get<AuthService>(AuthService);
   });
 
-  it('should return data from the service', async () => {
-    const controllerResponse = controller.getGithubAuthServerUrl();
-    const serviceResponse = authService.getGithubAuthUrl();
-    expect(controllerResponse.authUrl).toEqual(serviceResponse);
-    expect(authService.getGithubAuthUrl).toHaveBeenCalled();
+  describe('Get Github Auth Server Url', () => {
+    it('should return data from the service', async () => {
+      jest
+        .spyOn(authService, 'getGithubAuthUrl')
+        .mockReturnValue('github auth url');
+      const controllerResponse = controller.getGithubAuthServerUrl();
+      const serviceResponse = authService.getGithubAuthUrl();
+      expect(controllerResponse.authUrl).toEqual(serviceResponse);
+      expect(authService.getGithubAuthUrl).toHaveBeenCalled();
+    });
+  });
+
+  describe('Github Authentication', () => {
+    it('should return 401 response when getAccessToken throws an error', async () => {
+      jest
+        .spyOn(authService, 'getAccessToken')
+        .mockRejectedValue(new Error('Authentication failed'));
+
+      try {
+        await controller.githubAuthentication({ authCode: 'authCode' });
+      } catch (error) {
+        expect(error).toBeInstanceOf(UnauthorizedException);
+        expect(error.response).toEqual({
+          statusCode: 401,
+          message: 'Unauthorized',
+        });
+      }
+    });
   });
 });
--- backend/src/auth/controller/auth.controller.ts
+++ backend/src/auth/controller/auth.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AuthService } from '../service/auth.service';
 
 @Controller('auth')
@@ -7,5 +13,19 @@ export class AuthController {
   @Get('github/authorization-server')
   getGithubAuthServerUrl() {
     return { authUrl: this.authService.getGithubAuthUrl() };
+  }
+  @Post('github/authentication')
+  async githubAuthentication(@Body() body: { authCode: string }) {
+    try {
+      const accessToken = await this.authService.getAccessToken(body.authCode);
+    } catch {
+      throw new UnauthorizedException();
+    }
+
+    // Todo: access token -> user infomation
+
+    // Todo: if github user is not member, store user infomation in temp user table and return tempIdToken
+
+    // Todo: if github user is member, login
   }
 }
