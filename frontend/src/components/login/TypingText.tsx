3ac5ca9c8f335111a4d958f42d934503a6621338
dongind.oct <dongind.oct@gmail.com>
2024-02-26 21:43:37 +0900

    typing animation : timeStamp를 통한 쓰로틀 적용
    1. requestAnimationFrame은 callback 함수에 실행되는 시점인 timestamp를 parameter로 전달함
    - 이를 이용하여, 실행되는 시점이 frame을 넘긴 시점에 애니메이션을 repaint 하도록 설정

--- frontend/src/components/login/TypingText.tsx
+++ frontend/src/components/login/TypingText.tsx
@@ -3,26 +3,22 @@ import { useEffect, useRef, useState } from "react";
 const TypingText = ({ text, frame }: { text: string; frame: number }) => {
   const [typingText, setTypingText] = useState<string>("");
   const textIndex = useRef<number>(0);
-  // useEffect(() => {
-  //   let timer = setInterval(() => {
-  //     setTypingText((state) => {
-  //       // console.log("setInterval 실행!");
-  //       if (text.length <= textIndex.current) return state;
-  //       const newState = state + text[textIndex.current];
-  //       textIndex.current += 1;
-  //       return newState;
-  //     });
-  //   }, frame);
-  //   return () => clearInterval(timer);
-  // }, [text]);
+  const lastTimeStamp = useRef<number | null>(null);
+  const animationCallback = (timeStamp: number) => {
+    if (lastTimeStamp.current === null) {
+      lastTimeStamp.current = timeStamp;
+    }
 
-  const animationCallback = () => {
-    console.log("animation callback start!");
-    setTypingText((state) => {
-      const newState = state + text[textIndex.current];
-      textIndex.current += 1;
-      return newState;
-    });
+    const elapsedTime = timeStamp - lastTimeStamp.current;
+
+    if (elapsedTime > frame) {
+      lastTimeStamp.current = timeStamp;
+      setTypingText((state) => {
+        const newState = state + text[textIndex.current];
+        textIndex.current += 1;
+        return newState;
+      });
+    }
 
     if (textIndex.current >= text.length - 1) return;
     requestAnimationFrame(animationCallback);
