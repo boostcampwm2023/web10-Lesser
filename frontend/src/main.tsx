dc8260d1ea40ce1415d055ffef94e12de259af04
dongind.oct <dongind.oct@gmail.com>
2024-02-26 18:59:07 +0900

    Typing Animation : strict 모드 해제 후 결과 확인
    1. strict 모드 해제 후 애니메이션이 정상적으로 출력되는 것을 확인

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -31,8 +31,8 @@ async function enableMocking() {
 
 enableMocking().then(() => {
   ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>
+    // <React.StrictMode>
+    <RouterProvider router={router} />
+    // </React.StrictMode>
   );
 });
