ef714eb7ea2372e8f9833d3888e087109f390a7f
dongind.oct <dongind.oct@gmail.com>
2024-02-26 19:50:08 +0900

    typing animation : requestAnimationFrame으로 애니메이션 변경

--- frontend/src/components/login/TypingText.tsx
+++ frontend/src/components/login/TypingText.tsx
@@ -3,16 +3,36 @@ import { useEffect, useRef, useState } from "react";
 const TypingText = ({ text, frame }: { text: string; frame: number }) => {
   const [typingText, setTypingText] = useState<string>("");
   const textIndex = useRef<number>(0);
+  // useEffect(() => {
+  //   let timer = setInterval(() => {
+  //     setTypingText((state) => {
+  //       // console.log("setInterval 실행!");
+  //       if (text.length <= textIndex.current) return state;
+  //       const newState = state + text[textIndex.current];
+  //       textIndex.current += 1;
+  //       return newState;
+  //     });
+  //   }, frame);
+  //   return () => clearInterval(timer);
+  // }, [text]);
+
+  const animationCallback = () => {
+    setTypingText((state) => {
+      const newState = state + text[textIndex.current];
+      textIndex.current += 1;
+      return newState;
+    });
+
+    if (textIndex.current >= text.length) return;
+    requestAnimationFrame(animationCallback);
+  };
+
   useEffect(() => {
-    let timer = setInterval(() => {
-      setTypingText((state) => {
-        if (text.length <= textIndex.current) return state;
-        const newState = state + text[textIndex.current];
-        textIndex.current += 1;
-        return newState;
-      });
-    }, frame);
-    return () => clearInterval(timer);
+    let animeId = requestAnimationFrame(animationCallback);
+
+    return () => {
+      cancelAnimationFrame(animeId);
+    };
   }, [text]);
   return <p>{typingText}</p>;
 };
