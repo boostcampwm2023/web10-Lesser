d8cd5da6496224eab6e8575ea2cb268abe982413
dongind.oct <dongind.oct@gmail.com>
2024-03-06 18:24:09 +0900

    MSW 임시 제거
    - MSW를 활용하는 규칙에 대해 학습하고 제정할 필요성이 보임

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,16 +2,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-async function enableMocking() {
-  if (process.env.NODE_ENV !== "development") {
-    return;
-  }
+// async function enableMocking() {
+//   if (process.env.NODE_ENV !== "development") {
+//     return;
+//   }
 
-  const { worker } = await import("./mocks/browser.ts");
+//   const { worker } = await import("./mocks/browser.ts");
 
-  return worker.start({ onUnhandledRequest: "bypass" });
-}
+//   return worker.start({ onUnhandledRequest: "bypass" });
+// }
 
-enableMocking().then(() => {
-  ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
-});
+// enableMocking().then(() => {
+// });
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
