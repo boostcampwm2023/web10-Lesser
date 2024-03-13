3e9e070c61ab6b4ea297bb7ce36e5e08f7b39dfc
조영우 <choyoungwoo9@naver.com>
2024-03-13 10:28:17 +0900

    feat: availability username 컨트롤러
    - username 유효성 검사 컨트롤러 구현
    - 유효시 {available: true} 반환
    - 유효성 검사 실패시 {available: false, massage: 에러 메시지} 반환

--- backend/src/member/member.module.ts
+++ backend/src/member/member.module.ts
@@ -3,10 +3,12 @@ import { MemberService } from './service/member.service';
 import { Member } from './entity/member.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MemberRepository } from './repository/member.repository';
+import { MemberController } from './controller/member.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Member])],
   providers: [MemberService, MemberRepository],
   exports: [MemberService],
+  controllers: [MemberController],
 })
 export class MemberModule {}
