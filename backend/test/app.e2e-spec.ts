cdbcb3dcd039a72d9d090121d3f201a5fcbc2772
Suhjeong Kim <kimsj.py@gmail.com>
2024-03-15 16:46:43 +0900

    test: e2e 테스트코드 설정
    - (GET) /auth/github/authorization-server API에 대한 e2e 테스트코드 작성
    - supertest의 request를 사용해 HTTP 요청을 생성하고 response 값에 대한 검증을 수행
    - beforeEach 함수를 사용하여 테스트 전에 데이터베이스의 모든 테이블 레코드를 삭제
    - afterAll 함수를 사용하여 테스트 후 app 종료
    
    Co-Authored-By: 조영우 <66576231+choyoungwoo9@users.noreply.github.com>

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from 'src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -12,13 +12,18 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.setGlobalPrefix('api');
     await app.init();
   });
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/api')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Hello Lesser!');
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
