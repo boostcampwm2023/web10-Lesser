cd6528aa8bb72b7c22cc8ca6821bd42d3f055fa2
조영우 <choyoungwoo9@naver.com>
2024-03-16 11:22:40 +0900

    test: POST /api/auth/github/authentication에 대한 e2e테스트 작성
    Co-Authored-By: Suhjeong Kim <109324498+kimsj-git@users.noreply.github.com>

---
+++ backend/test/auth/github-authentication.e2e-spec.ts
@@ -0,0 +1,45 @@
+import * as request from 'supertest';
+import {
+  app,
+  githubApiService,
+  jwtTokenPattern,
+  saveMemberToDatabase,
+} from 'test/setup';
+
+describe('POST /api/auth/github/authentication', () => {
+  it('should return 209', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/api/auth/github/authentication')
+      .send({ authCode: 'authCode' });
+
+    expect(response.status).toBe(209);
+    expect(response.body.tempIdToken).toMatch(jwtTokenPattern);
+  });
+
+  it('should return 201', async () => {
+    await saveMemberToDatabase();
+    const response = await request(app.getHttpServer())
+      .post('/api/auth/github/authentication')
+      .send({ authCode: 'authCode' });
+
+    expect(response.status).toBe(201);
+    expect(response.body.accessToken).toMatch(jwtTokenPattern);
+  });
+
+  it('should return 400', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/api/auth/github/authentication')
+      .send({ invalidParameter: 'authCode' });
+
+    expect(response.status).toBe(400);
+  });
+
+  it('should return 401', async () => {
+    jest.spyOn(githubApiService, 'fetchAccessToken').mockResolvedValue({});
+    const response = await request(app.getHttpServer())
+      .post('/api/auth/github/authentication')
+      .send({ authCode: 'invalidAuthCode' });
+
+    expect(response.status).toBe(401);
+  });
+});
