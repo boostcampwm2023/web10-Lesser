e4977ec1137f40ef72cf60df54470e412b4b1317
Suhjeong Kim <kimsj.py@gmail.com>
2024-03-21 11:17:59 +0900

    test: 내 프로젝트 목록 요청시 더미데이터 반환하는 API에 대한 e2e 테스트 작성
    - projectFixtures 생성
    - GET /api/project 요청시 projectFixtures와 동일한 값을 반환하는지 테스트
    - access token이 유효하지 않으면 401 에러 반환하는지 테스트
    
    Co-Authored-By: 조영우 <66576231+choyoungwoo9@users.noreply.github.com>

---
+++ backend/fixtures/project-fixtures.ts
@@ -0,0 +1,56 @@
+export const projectFixtures = [
+  {
+    id: 1,
+    title: 'Lesser',
+    createdAt: '2024-03-01T12:00:00Z',
+    currentSprint: {
+      title: '스프린트 1',
+      startDate: '2024-03-14T12:00:00Z',
+      endDate: '2024-03-21T12:00:00Z',
+      progressPercentage: 80,
+      myTasksLeft: 5,
+    },
+  },
+  {
+    id: 2,
+    title: 'Chikorita',
+    createdAt: '2024-03-02T12:00:00Z',
+    currentSprint: null,
+  },
+  {
+    id: 3,
+    title: 'Charmander',
+    createdAt: '2024-03-03T12:00:00Z',
+    currentSprint: {
+      title: '스프린트 1',
+      startDate: '2024-03-14T12:00:00Z',
+      endDate: '2024-03-21T12:00:00Z',
+      progressPercentage: 100,
+      myTasksLeft: 0,
+    },
+  },
+  {
+    id: 4,
+    title: 'Bulbasaur',
+    createdAt: '2024-03-04T12:00:00Z',
+    currentSprint: {
+      title: '스프린트 1',
+      startDate: '2024-03-14T12:00:00Z',
+      endDate: '2024-03-21T12:00:00Z',
+      progressPercentage: 15,
+      myTasksLeft: 36,
+    },
+  },
+  {
+    id: 5,
+    title: 'Squirtle',
+    createdAt: '2024-03-05T12:00:00Z',
+    currentSprint: {
+      title: '스프린트 1',
+      startDate: '2024-03-14T12:00:00Z',
+      endDate: '2024-03-21T12:00:00Z',
+      progressPercentage: 0,
+      myTasksLeft: 17,
+    },
+  },
+];
---
+++ backend/test/project/get-project.e2e-spec.ts
@@ -0,0 +1,41 @@
+import * as request from 'supertest';
+import { app, createMember, memberFixture } from 'test/setup';
+import { projectFixtures } from 'fixtures/project-fixtures';
+
+describe('GET /api/project', () => {
+  it('should return 200', async () => {
+    const { accessToken } = await createMember(memberFixture);
+
+    const response = await request(app.getHttpServer())
+      .get('/api/project')
+      .set('Authorization', `Bearer ${accessToken}`);
+
+    expect(response.status).toBe(200);
+    expect(response.body.projects).toEqual(projectFixtures);
+  });
+
+  it('should return 401 (Authorization header is missing)', async () => {
+    const response = await request(app.getHttpServer()).get('/api/project');
+
+    expect(response.status).toBe(401);
+    expect(response.body.message).toBe('Authorization header is missing');
+  });
+
+  it('should return 401 (Invalid authorization header format)', async () => {
+    const response = await request(app.getHttpServer())
+      .get('/api/project')
+      .set('Authorization', `accessToken`);
+
+    expect(response.status).toBe(401);
+    expect(response.body.message).toBe('Invalid authorization header format');
+  });
+
+  it('should return 401 Expired:accessToken', async () => {
+    const response = await request(app.getHttpServer())
+      .get('/api/project')
+      .set('Authorization', `Bearer accessToken`);
+
+    expect(response.status).toBe(401);
+    expect(response.body.message).toBe('Expired:accessToken');
+  });
+});
