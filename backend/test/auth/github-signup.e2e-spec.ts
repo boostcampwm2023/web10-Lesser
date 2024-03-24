81130ee07165b0a3947c1d0e22edcec74ac3c871
조영우 <choyoungwoo9@naver.com>
2024-03-24 10:49:39 +0900

    test: POST /api/auth/github/signup 에 대한 e2e 테스트 작성
    - 유효한 tempId토큰이고 body의 데이터 형식이 적절할때 성공적으로 회원가입
    - body의 데이터 형식이 적절하지 않을때 400에러를 반환
    - authorization 헤더가 없을때 401에러를 반환
    - authorization 헤더의 포맷이 적절하지 않을때 401에러를 반환
    - tempId토큰이 유효하지 않을때 401에러를 반환

---
+++ backend/test/auth/github-signup.e2e-spec.ts
@@ -0,0 +1,71 @@
+import * as request from 'supertest';
+import { app, jwtTokenPattern, memberFixture } from 'test/setup';
+
+describe('POST /api/auth/github/signup', () => {
+  const memberSignupPayload = {
+    username: memberFixture.username,
+    position: memberFixture.position,
+    techStack: memberFixture.tech_stack.stacks,
+  };
+  it('should return 201', async () => {
+    const authenticationResponse = await request(app.getHttpServer())
+      .post('/api/auth/github/authentication')
+      .send({ authCode: 'authCode' });
+
+    const response = await request(app.getHttpServer())
+      .post('/api/auth/github/signup')
+      .set('Authorization', `Bearer ${authenticationResponse.body.tempIdToken}`)
+      .send(memberSignupPayload);
+
+    expect(response.status).toBe(201);
+    expect(response.body.accessToken).toMatch(jwtTokenPattern);
+    // 추후 적용
+    // expect(response.body.member).toEqual({
+    //   username: memberFixture.username,
+    //   imageUrl: memberFixture.github_image_url,
+    // });
+    const [cookie] = response.headers['set-cookie'];
+    expect(cookie).toBeDefined();
+    const [, refreshToken] = cookie.match(/refreshToken=([^;]+)/);
+    expect(refreshToken).toMatch(jwtTokenPattern);
+  });
+
+  it('should return 400', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/api/auth/github/signup')
+      .send({
+        invalidProperty: 'invalidProperty',
+      });
+
+    expect(response.status).toBe(400);
+  });
+
+  it('should return 401 (Authorization header is missing)', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/api/auth/github/signup')
+      .send(memberSignupPayload);
+
+    expect(response.status).toBe(401);
+    expect(response.body.message).toBe('Authorization header is missing');
+  });
+
+  it('should return 401 (Invalid authorization header format)', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/api/auth/github/signup')
+      .set('Authorization', `No Bearer tempIdToken`)
+      .send(memberSignupPayload);
+
+    expect(response.status).toBe(401);
+    expect(response.body.message).toBe('Invalid authorization header format');
+  });
+
+  it('should return 401 (Expired:tempIdToken)', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/api/auth/github/signup')
+      .set('Authorization', `Bearer invalidToken`)
+      .send(memberSignupPayload);
+
+    expect(response.status).toBe(401);
+    expect(response.body.message).toBe('Expired:tempIdToken');
+  });
+});
