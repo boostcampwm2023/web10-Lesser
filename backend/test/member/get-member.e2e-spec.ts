867296292268116ac6ba94481bc1fdc8ad668620
조영우 <choyoungwoo9@naver.com>
2024-03-24 09:48:35 +0900

    test: setup 모듈에서 유저를 생성하는 메서드 변경
    - 유저를 생성하는 서비스를 사용하지 않고 API를 사용해 유저를 생성하도록 변경

--- backend/test/member/get-member.e2e-spec.ts
+++ backend/test/member/get-member.e2e-spec.ts
@@ -1,16 +1,16 @@
 import * as request from 'supertest';
-import { app, githubApiService, createMember, memberFixture } from 'test/setup';
+import { app, createMember, memberFixture } from 'test/setup';
 
 describe('GET /api/member', () => {
   it('should return 200', async () => {
-    const { accessToken, member } = await createMember(memberFixture);
+    const { accessToken } = await createMember(memberFixture, app);
     const response = await request(app.getHttpServer())
       .get('/api/member')
       .set('Authorization', `Bearer ${accessToken}`);
 
     expect(response.status).toBe(200);
-    expect(response.body.username).toBe(member.username);
-    expect(response.body.imageUrl).toBe(member.github_image_url);
+    expect(response.body.username).toBe(memberFixture.username);
+    expect(response.body.imageUrl).toBe(memberFixture.github_image_url);
   });
 
   it('should return 401 (Authorization header is missing)', async () => {
