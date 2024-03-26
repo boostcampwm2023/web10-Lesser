2a4a3d3beeaad95e4b86765eb46e984b3d462448
surinkwon <tnfls7070@gmail.com>
2024-03-26 21:51:48 +0900

    refactor: 프로젝트 정렬 함수 테스트 코드 수정
    변경된 함수 형식에 맞게 테스트 코드 수정

--- frontend/src/test/projectSortingTest.test.tsx
+++ frontend/src/test/projectSortingTest.test.tsx
@@ -1,70 +1,85 @@
-import { PROJECT_LIST_OPTION } from "../constants/projects";
+import { PROJECT_SORT_OPTION } from "../constants/projects";
+import { ProjectDTO } from "../types/projectDTO";
+import projectSortByOption from "../utils/projectSortByOption";
 
-interface Project {
-  id: number;
-  currentSprint?: { startDate: string };
-  createdAt: string;
-}
-const mockProjectListData: Project[] = [
+const mockProjectListData: ProjectDTO[] = [
   {
     id: 1,
-    currentSprint: { startDate: "2024-03-11T12:00:00Z" },
+    title: "",
+    currentSprint: {
+      title: "",
+      startDate: "2024-03-11T12:00:00Z",
+      endDate: "",
+      progressPercentage: 0,
+      myTasksLeft: 0,
+    },
     createdAt: "2024-03-01T12:00:00Z",
   },
   {
     id: 2,
-    currentSprint: { startDate: "2024-03-06T12:00:00Z" },
+    title: "",
+    currentSprint: {
+      title: "",
+      startDate: "2024-03-06T12:00:00Z",
+      endDate: "",
+      progressPercentage: 0,
+      myTasksLeft: 0,
+    },
     createdAt: "2024-03-02T12:00:00Z",
   },
   {
     id: 3,
+    title: "",
+    currentSprint: null,
     createdAt: "2024-03-05T12:00:00Z",
   },
   {
     id: 4,
+    title: "",
+    currentSprint: null,
     createdAt: "2024-03-01T12:00:00Z",
   },
   {
     id: 5,
-    currentSprint: { startDate: "2024-03-12T12:00:00Z" },
+    title: "",
+    currentSprint: {
+      title: "",
+      startDate: "2024-03-12T12:00:00Z",
+      endDate: "",
+      progressPercentage: 0,
+      myTasksLeft: 0,
+    },
     createdAt: "2024-03-10T12:00:00Z",
   },
 ];
 
 describe("프로젝트 목록 정렬 함수", () => {
-  let selectedOption: string;
-  const now = -new Date();
-  const sortingFunction = (a: Project, b: Project) => {
-    if (selectedOption === PROJECT_LIST_OPTION[0]) {
-      const aStartDate = a.currentSprint
-        ? new Date(a.currentSprint?.startDate)
-        : now;
-      const bStartDate = b.currentSprint
-        ? new Date(b.currentSprint?.startDate)
-        : now;
-
-      if (aStartDate === bStartDate) {
-        return Number(new Date(b.createdAt)) - Number(new Date(a.createdAt));
-      }
-
-      return Number(bStartDate) - Number(aStartDate);
-    } else {
-      return Number(new Date(b.createdAt)) - Number(new Date(a.createdAt));
-    }
-  };
+  const earliest = -new Date();
 
   it("옵션이 업데이트 순일 때", () => {
-    [selectedOption] = PROJECT_LIST_OPTION;
     const sortingResult = structuredClone(mockProjectListData)
-      .sort(sortingFunction)
+      .sort((projectA, projectB) =>
+        projectSortByOption({
+          projectA,
+          projectB,
+          option: PROJECT_SORT_OPTION.UPDATE,
+          earliest,
+        })
+      )
       .map((project) => project.id);
     expect(sortingResult).toEqual([5, 1, 2, 3, 4]);
   });
 
   it("옵션이 최신 순일 때", () => {
-    [, selectedOption] = PROJECT_LIST_OPTION;
     const sortingResult = structuredClone(mockProjectListData)
-      .sort(sortingFunction)
+      .sort((projectA, projectB) =>
+        projectSortByOption({
+          projectA,
+          projectB,
+          option: PROJECT_SORT_OPTION.RECENT,
+          earliest,
+        })
+      )
       .map((project) => project.id);
     expect(sortingResult).toEqual([5, 3, 2, 1, 4]);
   });
