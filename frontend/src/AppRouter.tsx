1ed11ec5d341096cbf7524d012c8e0d7aac8651d
surinkwon <tnfls7070@gmail.com>
2024-04-02 18:22:42 +0900

    feat: Router에 ProjectCreatePage 추가

--- frontend/src/AppRouter.tsx
+++ frontend/src/AppRouter.tsx
@@ -1,20 +1,32 @@
-import { Outlet, RouteObject, RouterProvider, createBrowserRouter } from "react-router-dom";
-import { LoginPage, SignupPage, AuthPage, TempHomepage } from "./pages";
+import {
+  Outlet,
+  RouteObject,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
+import {
+  LoginPage,
+  SignupPage,
+  AuthPage,
+  TempHomepage,
+  ProjectsPage,
+  ProjectCreatePage,
+} from "./pages";
 import { ROUTER_URL } from "./constants/path";
-import ProjectsPage from "./pages/projects/ProjectsPage";
 import GlobalErrorBoundary from "./GlobalErrorBoundary";
 import PrivateRoute from "./components/common/route/PrivateRoute";
 import PublicRoute from "./components/common/route/PublicRoute";
 
 type RouteType = "PRIVATE" | "PUBLIC";
 
-const createAuthCheckRouter = (routeType: RouteType, children: RouteObject[]) => {
-  const authCheckRouter = children.map((child: RouteObject) => {
-    return {
-      element: routeType === "PRIVATE" ? <PrivateRoute /> : <PublicRoute />,
-      children: [child],
-    };
-  });
+const createAuthCheckRouter = (
+  routeType: RouteType,
+  children: RouteObject[]
+) => {
+  const authCheckRouter = children.map((child: RouteObject) => ({
+    element: routeType === "PRIVATE" ? <PrivateRoute /> : <PublicRoute />,
+    children: [child],
+  }));
   return authCheckRouter;
 };
 
@@ -50,13 +62,15 @@ const router = createBrowserRouter([
           path: ROUTER_URL.PROJECTS,
           element: <ProjectsPage />,
         },
+        {
+          path: ROUTER_URL.PROJECTS_CREATE,
+          element: <ProjectCreatePage />,
+        },
       ]),
     ],
   },
 ]);
 
-const AppRouter = () => {
-  return <RouterProvider router={router} />;
-};
+const AppRouter = () => <RouterProvider router={router} />;
 
 export default AppRouter;
