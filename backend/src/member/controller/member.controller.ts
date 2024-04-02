0f63a002f5304a6328f3894c5a2118437773f5c3
Suhjeong Kim <kimsj.py@gmail.com>
2024-04-02 15:50:39 +0900

    refactor: 컨트롤러에서 반복되는 에러처리 로직 제거
    Co-Authored-By: 조영우 <66576231+choyoungwoo9@users.noreply.github.com>

--- backend/src/member/controller/member.controller.ts
+++ backend/src/member/controller/member.controller.ts
@@ -3,7 +3,6 @@ import {
   Get,
   Query,
   BadRequestException,
-  InternalServerErrorException,
   Req,
   UnauthorizedException,
 } from '@nestjs/common';
@@ -22,22 +21,17 @@ export class MemberController {
     } catch (err) {
       if (err.message === 'duplicate username')
         return { available: false, message: err.message };
-      throw new InternalServerErrorException(err.message);
     }
   }
+
   @Get('/')
   async getMember(@Req() request: BearerTokenRequest) {
     const accessToken = request.token;
     if (!accessToken)
       throw new UnauthorizedException('Bearer Token is missing');
-    try {
-      const { username, githubImageUrl } =
-        await this.memberService.getMemberPublicInfo(accessToken);
-      return { username, imageUrl: githubImageUrl };
-    } catch (err) {
-      if (err.message === 'Failed to verify token')
-        throw new UnauthorizedException('Expired:accessToken');
-      throw new InternalServerErrorException(err.message);
-    }
+
+    const { username, githubImageUrl } =
+      await this.memberService.getMemberPublicInfo(accessToken);
+    return { username, imageUrl: githubImageUrl };
   }
 }
