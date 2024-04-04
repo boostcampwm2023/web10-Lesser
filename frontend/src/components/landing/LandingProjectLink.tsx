3a71e9d696f4d57b6e1fa1f06596cd58f739ea1b
dongind.oct <dongind.oct@gmail.com>
2024-04-04 15:24:44 +0900

    refactor : LandingProject 컴포넌트 분리
    - 컴포넌트를 작은 단위로 분리

---
+++ frontend/src/components/landing/LandingProjectLink.tsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import { LANDING_PROJECT_LINK } from "../../constants/landing";
+import { LINK_URL } from "../../constants/path";
+
+interface LandingProjectLinkProps {
+  projectId: string;
+  type: "BACKLOG" | "SPRINT" | "SETTINGS";
+}
+
+const LandingProjectLink = ({ projectId, type }: LandingProjectLinkProps) => {
+  const { color, text, Icon } = LANDING_PROJECT_LINK[type];
+  return (
+    <Link
+      to={LINK_URL.BACKLOG(projectId)}
+      className={`w-[8.75rem] h-[5rem] border rounded-lg flex justify-center gap-2 items-center ${color} hover:shadow-button`}
+    >
+      <Icon height={36} width={36} fill="#FFFFFF" />
+      <div className="flex flex-col gap-1 text-white text-[1rem] font-bold">
+        <p>{text}</p>
+        <p>바로가기</p>
+      </div>
+    </Link>
+  );
+};
+
+export default LandingProjectLink;
--- frontend/src/pages/landing/LandingPage.tsx
+++ frontend/src/pages/landing/LandingPage.tsx
@@ -1,9 +1,7 @@
 import { useState } from "react";
 import { LandingDTO } from "../../types/DTO/landingDTO";
-import formatDate from "../../utils/formatDate";
-import { Link, useParams } from "react-router-dom";
-import { LINK_URL } from "../../constants/path";
-import { LANDING_PROJECT_LINK } from "../../constants/landing";
+import { useParams } from "react-router-dom";
+import LandingProject from "../../components/landing/\bLandingProject";
 
 const sampleData: LandingDTO = {
   project: {
@@ -35,27 +33,6 @@ const sampleData: LandingDTO = {
   ], // 외부 링크가 없는 경우 빈 배열 []
 };
 
-interface LandingProjectLinkProps {
-  projectId: string;
-  type: "BACKLOG" | "SPRINT" | "SETTINGS";
-}
-
-const LandingProjectLink = ({ projectId, type }: LandingProjectLinkProps) => {
-  const { color, text, Icon } = LANDING_PROJECT_LINK[type];
-  return (
-    <Link
-      to={LINK_URL.BACKLOG(projectId)}
-      className={`w-[8.75rem] h-[5rem] border rounded-lg flex justify-center gap-2 items-center ${color} hover:shadow-button`}
-    >
-      <Icon height={36} width={36} fill="#FFFFFF" />
-      <div className="flex flex-col gap-1 text-white text-[1rem] font-bold">
-        <p>{text}</p>
-        <p>바로가기</p>
-      </div>
-    </Link>
-  );
-};
-
 const LandingPage = () => {
   const [landingData] = useState<LandingDTO>(sampleData);
   const { projectId } = useParams();
@@ -64,18 +41,7 @@ const LandingPage = () => {
   return (
     <div className="h-full w-full flex flex-col justify-between">
       <div className="h-[17.6875rem] w-full shrink-0 flex gap-9">
-        <div className="border w-full p-6 flex flex-col justify-between shadow-box rounded-lg">
-          <div className="flex justify-between items-baseline text-middle-green font-bold">
-            <p className="text-xl">| {project.title}</p>
-            <p className="text-xs">{formatDate(project.createdAt)}</p>
-          </div>
-          <div className="text-xs">{project.subject}</div>
-          <div className="flex justify-between">
-            <LandingProjectLink projectId={projectId} type="BACKLOG" />
-            <LandingProjectLink projectId={projectId} type="SPRINT" />
-            <LandingProjectLink projectId={projectId} type="SETTINGS" />
-          </div>
-        </div>
+        <LandingProject {...{ project, projectId }} />
         <div className="border w-full"></div>
       </div>
       <div className="h-[20.5625rem] w-full shrink-0 flex gap-9">
