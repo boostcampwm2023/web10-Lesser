84f380bd27b27de98caa4c88374bccd9f673e149
dongind.oct <dongind.oct@gmail.com>
2024-04-04 15:27:44 +0900

    fix : 위치 확인을 위한 border 클래스 제거

--- frontend/src/components/landing/LandingProjectLink.tsx
+++ frontend/src/components/landing/LandingProjectLink.tsx
@@ -12,7 +12,7 @@ const LandingProjectLink = ({ projectId, type }: LandingProjectLinkProps) => {
   return (
     <Link
       to={LINK_URL.BACKLOG(projectId)}
-      className={`w-[8.75rem] h-[5rem] border rounded-lg flex justify-center gap-2 items-center ${color} hover:shadow-button`}
+      className={`w-[8.75rem] h-[5rem] rounded-lg flex justify-center gap-2 items-center ${color} hover:shadow-button`}
     >
       <Icon height={36} width={36} fill="#FFFFFF" />
       <div className="flex flex-col gap-1 text-white text-[1rem] font-bold">
