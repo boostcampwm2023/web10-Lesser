ee33d63247f1cb4057a63f11b825328e0dcd2946
dongind.oct <dongind.oct@gmail.com>
2024-04-04 14:33:56 +0900

    feat : Landing Page DTO 정리
    - Landing Page에서 다루기 될 서버 상태의 타입 정리

--- frontend/src/types/common/common.ts
+++ frontend/src/types/common/common.ts
@@ -3,3 +3,13 @@ import { FunctionComponent, SVGProps } from "react";
 export type SVGReactElement = FunctionComponent<
   SVGProps<SVGSVGElement> & { title?: string | undefined }
 >;
+
+type SingleInteger = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0;
+type SingleNatural = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+type TimeNumber = 0 | 1 | 2 | 3 | 4 | 5;
+type ISOStringYear = `${SingleNatural}${SingleInteger}${SingleInteger}${SingleInteger}`;
+type ISOStringMonth = `0${SingleNatural}` | `1${0 | 1 | 2}`;
+type ISOStringDate = `0${SingleInteger}`;
+type ISOStringHour = `${0 | 1}${SingleInteger}` | `2${0 | 1 | 2 | 3}`;
+type ISOStringMinuteSecond = `${TimeNumber}${SingleInteger}`;
+export type ISOStringType = `${ISOStringYear}-${ISOStringMonth}-${ISOStringDate}T${ISOStringHour}:`;
