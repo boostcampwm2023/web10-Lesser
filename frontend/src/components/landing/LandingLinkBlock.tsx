056b185b69a4ff471c6fa1bf6f3ee7c388fad3b8
dongind.oct <dongind.oct@gmail.com>
2024-04-08 19:33:46 +0900

    refactor :LandingLinkBlock button -> a로 변경
    - 태그의 의미를 살리기 위해 button 태그를 a 태그로 변경

--- frontend/src/components/landing/LandingLinkBlock.tsx
+++ frontend/src/components/landing/LandingLinkBlock.tsx
@@ -4,20 +4,18 @@ import getLinkType from "../../utils/getLinkType";
 import ProfileImage from "../common/ProfileImage";
 
 const LandingLinkBlock = ({ description, url }: LandingLinkDTO) => {
-  const handleLinkClick = () => {
-    window.open(url);
-  };
   const linkLogoUrl = LINK_LOGO_URL[getLinkType(url)];
   return (
-    <button
+    <a
       className="w-full flex justify-start items-center gap-4 p-3 bg-white rounded-lg shadow-box hover:bg-light-gray"
-      onClick={handleLinkClick}
+      href={url}
+      target="_blank"
     >
       <ProfileImage imageUrl={linkLogoUrl} pxSize={40} />
       <p className="text-dark-green text-xs font-bold truncate">
         {description}
       </p>
-    </button>
+    </a>
   );
 };
 
