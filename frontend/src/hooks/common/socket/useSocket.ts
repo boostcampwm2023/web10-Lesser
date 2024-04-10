7e4ef285333f14d71af85c61f5ffa0a09f75a354
dongind.oct <dongind.oct@gmail.com>
2024-04-10 10:44:08 +0900

    feat : useSocket 커스텀 훅 작성
    - useSocket에서 Socket.io를 호출하여, 서버와 소켓 연결 시도
    - 연결 여부 및 socket 객체를 반환하여 각 페이지에 socket 객체를 전달

---
+++ frontend/src/hooks/common/socket/useSocket.ts
@@ -0,0 +1,30 @@
+import { io } from "socket.io-client";
+import { BASE_URL } from "../../../constants/path";
+import { useEffect, useState } from "react";
+
+const useSocket = (projectId: string) => {
+  const WS_URL = `${BASE_URL}/project-${projectId}`;
+  const socket = io(WS_URL);
+  const [connected, setConnected] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleOnConnect = () => {
+      setConnected(true);
+    };
+    const handleOnDisconnect = () => {
+      setConnected(false);
+    };
+
+    socket.on("connect", handleOnConnect);
+    socket.on("disconnect", handleOnDisconnect);
+
+    return () => {
+      socket.off("connect", handleOnConnect);
+      socket.off("disconnect", handleOnDisconnect);
+    };
+  }, []);
+
+  return { socket, connected };
+};
+
+export default useSocket;
--- frontend/src/pages/main/MainPage.tsx
+++ frontend/src/pages/main/MainPage.tsx
@@ -1,15 +1,17 @@
 import { Outlet, useLocation, useParams } from "react-router-dom";
 import ProjectSidebar from "../../components/main/ProjectSidebar";
+import useSocket from "../../hooks/common/socket/useSocket";
 
 const MainPage = () => {
   const { pathname } = useLocation();
   const { projectId } = useParams();
   if (!projectId) throw Error("잘못된 ProjectID 입니다.");
+  const { socket } = useSocket(projectId);
   return (
     <div className="flex justify-center items-center h-screen min-w-[76rem] gap-9">
       <ProjectSidebar {...{ pathname, projectId }} />
       <div className="h-[40.5rem] min-w-[67.9375rem]">
-        <Outlet />
+        <Outlet context={{ socket }} />
       </div>
     </div>
   );
