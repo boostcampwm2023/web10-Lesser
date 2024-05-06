f3c83e540a25d013a79f235bc5f302a424028906
조영우 <choyoungwoo9@naver.com>
2024-05-06 11:25:25 +0900

    test: 메모 생성 API e2e 테스트코드 추가
    - 회원 2명이 랜딩 페이지에 접속한 상태에서 메모 생성시 모든 회원이 생성된 메모 데이터를 받는 테스트 추가
    - 잘못된 메모 데이터 전송시 에러 수신하는 테스트 추가
    
    Co-Authored-By: Suhjeong Kim <109324498+kimsj-git@users.noreply.github.com>

---
+++ backend/test/project/ws-memo.e2e-spec.ts
@@ -0,0 +1,126 @@
+import {
+  app,
+  appInit,
+  connectServer,
+  createMember,
+  createProject,
+  getProjectLinkId,
+  joinProject,
+  memberFixture,
+  memberFixture2,
+  projectPayload,
+} from 'test/setup';
+
+describe('WS memo', () => {
+  beforeEach(async () => {
+    await app.close();
+    await appInit();
+    await app.listen(3000);
+  });
+
+  it('should return created memo data when received create memo request', async () => {
+    let socket1;
+    let socket2;
+    return new Promise<void>(async (resolve, reject) => {
+      // 회원1 회원가입 + 프로젝트 생성
+      const accessToken = (await createMember(memberFixture, app)).accessToken;
+      const project = await createProject(accessToken, projectPayload, app);
+      const projectLinkId = await getProjectLinkId(accessToken, project.id);
+
+      // 회원2 회원가입 + 프로젝트 참여
+      const accessToken2 = (await createMember(memberFixture2, app))
+        .accessToken;
+      await joinProject(accessToken2, projectLinkId);
+
+      socket1 = connectServer(project.id, accessToken);
+      await emitJoinLanding(socket1);
+      await initLanding(socket1);
+
+      socket2 = connectServer(project.id, accessToken2);
+      await emitJoinLanding(socket2);
+      await initLanding(socket2);
+
+      const requestData = {
+        action: 'create',
+        content: { color: 'yellow' },
+      };
+      socket1.emit('memo', requestData);
+      socket1.on('error', () => {
+        reject();
+      });
+      await Promise.all([
+        expectMemo(socket1, memberFixture.username, requestData.content.color),
+        expectMemo(socket2, memberFixture.username, requestData.content.color),
+      ]);
+      resolve();
+    }).finally(() => {
+      socket1.close();
+      socket2.close();
+    });
+  });
+
+  it('should return error message list when data format is invalid', async () => {
+    let socket;
+    return new Promise<void>(async (resolve) => {
+      const accessToken = (await createMember(memberFixture, app)).accessToken;
+      const project = await createProject(accessToken, projectPayload, app);
+
+      socket = connectServer(project.id, accessToken);
+      await emitJoinLanding(socket);
+      await initLanding(socket);
+
+      const requestData = {
+        action: 'create',
+        content: { color: 'invalidColor' },
+      };
+      socket.emit('memo', requestData);
+      socket.on('error', (data) => {
+        expect(data.errorList).toBeDefined();
+        expect(data.errorList.length).toBeGreaterThan(0);
+        resolve();
+      });
+    }).finally(() => {
+      socket.close();
+    });
+  });
+});
+
+const expectMemo = (socket, author, color) => {
+  return new Promise<void>((res) => {
+    socket.on('landing', (data) => {
+      const { content, action, domain } = data;
+      expect(domain).toBe('memo');
+      expect(action).toBe('create');
+      expect(content.id).toBeDefined();
+      expect(content.title).toBeDefined();
+      expect(content.content).toBeDefined();
+      expect(content.createdAt).toBeDefined();
+      expect(content.author).toBe(author);
+      expect(content.color).toBe(color);
+      socket.off('landing');
+      res();
+    });
+  });
+};
+
+const emitJoinLanding = (socket) => {
+  return new Promise<void>((res, rej) => {
+    socket.on('connect', () => {
+      socket.emit('joinLanding');
+      socket.off('connect');
+      res();
+    });
+  });
+};
+
+const initLanding = (socket) => {
+  return new Promise<void>((res, rej) => {
+    socket.on('landing', (data) => {
+      const { action, content, domain } = data;
+      if (action === 'init') {
+        socket.off('landing');
+        res();
+      }
+    });
+  });
+};
