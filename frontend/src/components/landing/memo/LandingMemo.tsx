ec3ffabf9a982dd405787dd92de3c39c7736f596
dongind.oct <dongind.oct@gmail.com>
2024-05-06 10:51:33 +0900

    refactor : MemoEditor 컴포넌트화
    - Memo의 색상 변경 및 삭제하는 에디터를 별도의 컴포넌트로 분리
      - 이를 통해 코드의 가독성 향상 및 유지보수 용이를 목적으로 함

--- frontend/src/components/landing/memo/LandingMemo.tsx
+++ frontend/src/components/landing/memo/LandingMemo.tsx
@@ -1,4 +1,4 @@
-import TrashBinIcon from "../../../assets/icons/trash-can.svg?react";
+import MemoEditor from "./MemoEditor";
 
 interface LandingMemoBlockProps {
   username: string;
@@ -19,15 +19,7 @@ const LandingMemoBlock = ({ username }: LandingMemoBlockProps) => {
         />
         <p className="text-xxxs font-bold">{username}</p>
       </div>
-      <div className="absolute w-32 h-fit bottom-3 right-3 z-10 rounded-full flex justify-between items-center duration-100">
-        <button className="w-5 h-5 rounded-full border-white border bg-[#FFD966] hover:border-blue-500 hover:border-2 hover:scale-125 transition-all ease-in-out" />
-        <button className="w-5 h-5 rounded-full border-white border bg-[#FFAFA3] hover:border-blue-500 hover:border-2 hover:scale-125 transition-all ease-in-out" />
-        <button className="w-5 h-5 rounded-full border-white border bg-[#80CAFF] hover:border-blue-500 hover:border-2 hover:scale-125 transition-all ease-in-out" />
-        <button className="w-5 h-5 rounded-full border-white border bg-[#D9D9D9] hover:border-blue-500 hover:border-2 hover:scale-125 transition-all ease-in-out" />
-        <button className="w-5 h-5 rounded-full border-white border bg-error-red flex justify-center items-center hover:border-blue-500 hover:border-2 hover:scale-125 transition-all ease-in-out">
-          <TrashBinIcon width={12} height={12} color="#FFFFFF" />
-        </button>
-      </div>
+      <MemoEditor />
     </div>
   );
 };
