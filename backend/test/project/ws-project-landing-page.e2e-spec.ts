7e2e1251f91d242e0a8b05fa12020bfcf38d204d
조영우 <choyoungwoo9@naver.com>
2024-05-10 23:23:23 +0900

    test: 랜딩페이지 접속 API 프로젝트의 메모 테스트 추가
    - 메모를 생성한 후, 랜딩페이지에 접속했을때 생성한 메모를 받는지 확인하는 테스트 추가

--- backend/test/project/ws-project-landing-page.e2e-spec.ts
+++ backend/test/project/ws-project-landing-page.e2e-spec.ts
@@ -1,3 +1,4 @@
+import { Socket } from 'socket.io-client';
 import {
   app,
   appInit,
@@ -9,7 +10,7 @@ import {
 } from 'test/setup';
 
 describe('WS landing', () => {
-  let socket;
+  let socket: Socket;
 
   beforeEach(async () => {
     await app.close();
@@ -32,7 +33,8 @@ describe('WS landing', () => {
       });
 
       socket.on('landing', (data) => {
-        const { action, content } = data;
+        const { action, domain, content } = data;
+        expect(domain).toBe('landing');
         expect(action).toBe('init');
         expect(content.project.title).toBe(projectPayload.title);
         expect(content.project.subject).toBe(projectPayload.subject);
@@ -40,15 +42,95 @@ describe('WS landing', () => {
         expect(content.myInfo).toBeDefined();
         expect(content.member).toBeDefined();
         expect(content.sprint).toBeDefined();
-        expect(content.board).toBeDefined();
+        expect(content.memoList).toBeDefined();
         expect(content.link).toBeDefined();
         expect(content.inviteLinkId).toBeDefined();
         resolve();
       });
 
-      socket.on('connect_error', () => {
-        reject('connect_error fail');
+      handleConnectErrorWithReject(socket, reject);
+    });
+  });
+
+  it('should return created memoList in landing page data when project member has already created memo', async () => {
+    return await new Promise<void>(async (resolve, reject) => {
+      const accessToken = (await createMember(memberFixture, app)).accessToken;
+      const project = await createProject(accessToken, projectPayload, app);
+
+      //메모 생성
+      socket = connectServer(project.id, accessToken);
+      handleConnectErrorWithReject(socket, reject);
+      await emitJoinLanding(socket);
+      await initLanding(socket);
+      const requestData = {
+        action: 'create',
+        content: { color: 'yellow' },
+      };
+      socket.emit('memo', requestData);
+      await getCreateMemoMsg(socket);
+      socket.close();
+
+      //메모 검증
+      socket = connectServer(project.id, accessToken);
+      handleConnectErrorWithReject(socket, reject);
+      await emitJoinLanding(socket);
+
+      socket.on('landing', (data) => {
+        const { action, domain, content } = data;
+        const { memoList } = content;
+        expect(memoList.length).toBe(1);
+        const memo = memoList[0];
+        if (action === 'init' && domain === 'landing') {
+          expect(memo.id).toBeDefined();
+          expect(memo.title).toBe('');
+          expect(memo.content).toBe('');
+          expect(memo.author).toBe(memberFixture.username);
+          expect(memo.author).toBe(memberFixture.username);
+          expect(memo.createdAt).toBeDefined();
+          expect(memo.color).toBe(requestData.content.color);
+          resolve();
+        }
       });
     });
   });
 });
+
+const handleConnectErrorWithReject = (socket, reject) => {
+  socket.on('connect_error', () => {
+    reject('connect_error fail');
+  });
+};
+
+const emitJoinLanding = (socket) => {
+  return new Promise<void>((res, rej) => {
+    socket.on('connect', () => {
+      socket.emit('joinLanding');
+      socket.off('connect');
+      res();
+    });
+  });
+};
+
+const initLanding = (socket) => {
+  return new Promise<void>((res, rej) => {
+    socket.on('landing', (data) => {
+      const { action } = data;
+      if (action === 'init') {
+        socket.off('landing');
+        res();
+      }
+    });
+  });
+};
+
+const getCreateMemoMsg = (socket) => {
+  return new Promise<void>((res) => {
+    socket.on('landing', (data) => {
+      const { action, domain } = data;
+      expect(domain).toBe('memo');
+      expect(action).toBe('create');
+      socket.off('landing');
+      res();
+    });
+  });
+};
