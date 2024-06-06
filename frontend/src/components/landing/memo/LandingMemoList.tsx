e6f6377ba3831f86ad67c1a185d6c86b95628ae9
dongind.oct <dongind.oct@gmail.com>
2024-06-06 16:11:18 +0900

    refactor : MemoList 코드에 웹소켓 객체 직접 주입

--- frontend/src/components/landing/memo/LandingMemoList.tsx
+++ frontend/src/components/landing/memo/LandingMemoList.tsx
@@ -1,23 +1,85 @@
-import { LandingMemoDTO } from "../../../types/DTO/landingDTO";
-import { MemoColorType } from "../../../types/common/landing";
+import { Socket } from "socket.io-client";
+import { LandingDTO, LandingMemoDTO } from "../../../types/DTO/landingDTO";
+import {
+  LandingSocketData,
+  LandingSocketDomain,
+  LandingSocketMemoAction,
+  MemoColorType,
+} from "../../../types/common/landing";
 import LandingTitleUI from "../common/LandingTitleUI";
 import MemoBlock from "./MemoBlock";
+import { useOutletContext } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-interface LandingMemoListProps {
-  memoList: LandingMemoDTO[];
-  memoSocketEvent: {
-    emitMemoCreateEvent: () => void;
-    emitMemoDeleteEvent: (id: number) => void;
-    emitMemoColorUpdateEvent: (id: number, color: MemoColorType) => void;
+const LandingMemoList = () => {
+  const { socket }: { socket: Socket } = useOutletContext();
+  const [memoList, setMemoList] = useState<LandingMemoDTO[]>([]);
+
+  const handleInitEvent = (content: LandingDTO) => {
+    const { memoList } = content as LandingDTO;
+    setMemoList(memoList);
   };
-}
 
-const LandingMemoList = ({
-  memoList,
-  memoSocketEvent,
-}: LandingMemoListProps) => {
-  const { emitMemoCreateEvent, emitMemoDeleteEvent, emitMemoColorUpdateEvent } =
-    memoSocketEvent;
+  const handleMemoEvent = (
+    action: LandingSocketMemoAction,
+    content: LandingMemoDTO
+  ) => {
+    switch (action) {
+      case LandingSocketMemoAction.CREATE:
+        setMemoList((memoList: LandingMemoDTO[]) => {
+          return [content, ...memoList];
+        });
+        break;
+      case LandingSocketMemoAction.DELETE:
+        setMemoList((memoList: LandingMemoDTO[]) => {
+          return memoList.filter(
+            (memo: LandingMemoDTO) => memo.id !== content.id
+          );
+        });
+        break;
+      case LandingSocketMemoAction.COLOR_UPDATE:
+        setMemoList((memoList: LandingMemoDTO[]) => {
+          return memoList.map((memo: LandingMemoDTO) => {
+            if (memo.id !== content.id) return memo;
+            return { ...memo, color: content.color };
+          });
+        });
+    }
+  };
+  const handleOnMemoLanding = ({
+    domain,
+    action,
+    content,
+  }: LandingSocketData) => {
+    console.log(domain, action, content);
+    switch (domain) {
+      case LandingSocketDomain.INIT:
+        handleInitEvent(content);
+        break;
+      case LandingSocketDomain.MEMO:
+        console.log("memo editing");
+        handleMemoEvent(action, content);
+        break;
+    }
+  };
+  const emitMemoCreateEvent = () => {
+    socket.emit("memo", { action: "create", content: { color: "yellow" } });
+  };
+  const emitMemoDeleteEvent = (id: number) => {
+    socket.emit("memo", { action: "delete", content: { id } });
+  };
+  const emitMemoColorUpdateEvent = (id: number, color: MemoColorType) => {
+    socket.emit("memo", { action: "colorUpdate", content: { id, color } });
+  };
+
+  useEffect(() => {
+    socket.on("landing", handleOnMemoLanding);
+
+    return () => {
+      socket.off("landing");
+    };
+  }, [socket]);
+
   return (
     <div className="w-full rounded-lg shadow-box bg-gradient-to-tr from-dark-green-linear-from to-dark-green-linear-to">
       <div className="py-6 ps-6 pe-3 w-[32rem]">
--- frontend/src/pages/landing/LandingPage.tsx
+++ frontend/src/pages/landing/LandingPage.tsx
@@ -6,7 +6,6 @@ import LandingLink from "../../components/landing/link/LandingLink";
 import { Socket } from "socket.io-client";
 import useLandingSocket from "../../hooks/common/socket/useLandingSocket";
 import LandingMemoList from "../../components/landing/memo/LandingMemoList";
-import useLandingEmitEvent from "../../hooks/common/socket/useLandingEmitEvent";
 
 const LandingPage = () => {
   const { projectId } = useParams();
@@ -15,15 +14,13 @@ const LandingPage = () => {
   }
 
   const { socket }: { socket: Socket } = useOutletContext();
-  const { project, myInfo, member, memoList, inviteLinkIdRef } =
-    useLandingSocket(socket);
-  const { memoSocketEvent } = useLandingEmitEvent(socket);
+  const { project, myInfo, member, inviteLinkIdRef } = useLandingSocket(socket);
 
   return (
     <div className="flex flex-col justify-between w-full h-full">
       <div className="h-[17.6875rem] w-full shrink-0 flex gap-9">
         <LandingProject {...{ projectId }} />
-        <LandingMemoList {...{ memoList, memoSocketEvent }} />
+        <LandingMemoList />
       </div>
       <div className="h-[20.5625rem] w-full shrink-0 flex gap-9">
         <LandingSprint />
