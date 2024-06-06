dc981161707c3bd04ffebf7b1a50801e4f5db7ad
surinkwon <tnfls7070@gmail.com>
2024-06-06 20:01:58 +0900

    fix: 중복 연결 요청을 막기 위한 useSocket 로직 수정
    socket이 일반 변수로 관리되고 있어 연결 요청이 두 번 발생하던 현상 수정
    disconnect 동작을 추가해 프로젝트 페이지에서 벗어나면 연결 해제되도록 수정

--- frontend/src/hooks/common/socket/useSocket.ts
+++ frontend/src/hooks/common/socket/useSocket.ts
@@ -5,12 +5,15 @@ import { getAccessToken } from "../../../apis/utils/authAPI";
 
 const useSocket = (projectId: string) => {
   const WS_URL = `${BASE_URL}/project-${projectId}`;
-  const socket = io(WS_URL, {
-    path: `/api/socket.io`,
-    auth: {
-      accessToken: getAccessToken(),
-    },
-  });
+  const [socket] = useState(
+    io(WS_URL, {
+      path: `/api/socket.io`,
+      auth: {
+        accessToken: getAccessToken(),
+      },
+      autoConnect: false,
+    })
+  );
   const [connected, setConnected] = useState<boolean>(false);
 
   useEffect(() => {
@@ -21,10 +24,12 @@ const useSocket = (projectId: string) => {
       setConnected(false);
     };
 
+    socket.connect();
     socket.on("connect", handleOnConnect);
     socket.on("disconnect", handleOnDisconnect);
 
     return () => {
+      socket.disconnect();
       socket.off("connect", handleOnConnect);
       socket.off("disconnect", handleOnDisconnect);
     };
