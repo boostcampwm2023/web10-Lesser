10d625be0b013f7cb3a09b043bf8feea351e4e95
surinkwon <tnfls7070@gmail.com>
2024-06-13 10:52:43 +0900

    fix: memo 리스트 렌더링 최적화 에러 수정
    중복으로 처리되어 memo를 수정할 때 모든 컴포넌트가 렌더링 되는 문제 수정

--- frontend/src/hooks/common/socket/useLandingSocket.ts
+++ frontend/src/hooks/common/socket/useLandingSocket.ts
@@ -1,67 +1,26 @@
 import { Socket } from "socket.io-client";
 import { useEffect, useState } from "react";
-import {
-  LandingDTO,
-  LandingLinkDTO,
-  LandingMemoDTO,
-  LandingProjectDTO,
-  LandingSprintDTO,
-} from "../../../types/DTO/landingDTO";
+import { LandingDTO, LandingProjectDTO } from "../../../types/DTO/landingDTO";
 import { DEFAULT_VALUE } from "../../../constants/landing";
 import {
   LandingSocketData,
   LandingSocketDomain,
-  LandingSocketMemoAction,
 } from "../../../types/common/landing";
 
 const useLandingSocket = (socket: Socket) => {
   const [project, setProject] = useState<LandingProjectDTO>(
     DEFAULT_VALUE.PROJECT
   );
-  const [sprint, setSprint] = useState<LandingSprintDTO | null>(null);
-  const [memoList, setMemoList] = useState<LandingMemoDTO[]>([]);
-  const [link, setLink] = useState<LandingLinkDTO[]>([]);
 
   const handleInitEvent = (content: LandingDTO) => {
-    const { project, sprint, memoList, link } = content as LandingDTO;
+    const { project } = content as LandingDTO;
     setProject(project);
-    setSprint(sprint);
-    setMemoList(memoList);
-    setLink(link);
   };
 
-  const handleMemoEvent = (
-    action: LandingSocketMemoAction,
-    content: LandingMemoDTO
-  ) => {
-    switch (action) {
-      case LandingSocketMemoAction.CREATE:
-        setMemoList((memoList: LandingMemoDTO[]) => [content, ...memoList]);
-        break;
-      case LandingSocketMemoAction.DELETE:
-        setMemoList((memoList: LandingMemoDTO[]) =>
-          memoList.filter((memo: LandingMemoDTO) => memo.id !== content.id)
-        );
-        break;
-      case LandingSocketMemoAction.COLOR_UPDATE:
-        setMemoList((memoList: LandingMemoDTO[]) =>
-          memoList.map((memo: LandingMemoDTO) => {
-            if (memo.id !== content.id) {
-              return memo;
-            }
-            return { ...memo, color: content.color };
-          })
-        );
-    }
-  };
-
-  const handleOnLanding = ({ domain, action, content }: LandingSocketData) => {
+  const handleOnLanding = ({ domain, content }: LandingSocketData) => {
     switch (domain) {
       case LandingSocketDomain.INIT:
         handleInitEvent(content);
-        break;
-      case LandingSocketDomain.MEMO:
-        handleMemoEvent(action, content);
         break;
     }
   };
@@ -77,9 +36,6 @@ const useLandingSocket = (socket: Socket) => {
 
   return {
     project,
-    sprint,
-    memoList,
-    link,
   };
 };
 
