c531803c7a9b52ab21308b2b361f5c14540435cd
조영우 <choyoungwoo9@naver.com>
2024-06-13 11:06:51 +0900

    test: 프로젝트 외부링크 추가 API 테스트 추가

---
+++ backend/test/project/ws-link.e2e-spec.ts
@@ -0,0 +1,91 @@
+import {
+  app,
+  appInit,
+  connectServer,
+  createMember,
+  createProject,
+  getProjectLinkId,
+  joinProject,
+  memberFixture,
+  memberFixture2,
+  projectPayload,
+} from 'test/setup';
+import {
+  emitJoinLanding,
+  expectUpdatedMemberStatus,
+  handleConnectErrorWithReject,
+  handleErrorWithReject,
+  initLanding,
+  initLandingAndReturnId,
+} from './ws-common';
+
+describe('WS link', () => {
+  beforeEach(async () => {
+    await app.close();
+    await appInit();
+    await app.listen(3000);
+  });
+  describe('link create', () => {
+    it('should return created link data when received create link request', async () => {
+      let socket1;
+      let socket2;
+      return new Promise<void>(async (resolve, reject) => {
+        // 회원1 회원가입 + 프로젝트 생성
+        const accessToken = (await createMember(memberFixture, app))
+          .accessToken;
+        const project = await createProject(accessToken, projectPayload, app);
+        const projectLinkId = await getProjectLinkId(accessToken, project.id);
+
+        // 회원2 회원가입 + 프로젝트 참여
+        const accessToken2 = (await createMember(memberFixture2, app))
+          .accessToken;
+        await joinProject(accessToken2, projectLinkId);
+
+        socket1 = connectServer(project.id, accessToken);
+        handleConnectErrorWithReject(socket1, reject);
+        handleErrorWithReject(socket1, reject);
+        await emitJoinLanding(socket1);
+        await initLanding(socket1);
+
+        socket2 = connectServer(project.id, accessToken2);
+        handleConnectErrorWithReject(socket2, reject);
+        handleErrorWithReject(socket2, reject);
+        await emitJoinLanding(socket2);
+        const memberId2 = await initLandingAndReturnId(socket2);
+        await expectUpdatedMemberStatus(socket1, 'on', memberId2);
+
+        const url = '유효한 url';
+        const description = '피그마';
+        const requestData = {
+          action: 'create',
+          content: { url, description },
+        };
+        socket1.emit('link', requestData);
+        await Promise.all([
+          expectCreateLink(socket1, url, description),
+          expectCreateLink(socket2, url, description),
+        ]);
+        resolve();
+      }).finally(() => {
+        socket1.close();
+        socket2.close();
+      });
+    });
+
+    const expectCreateLink = (socket, url, description) => {
+      return new Promise<void>((resolve, reject) => {
+        socket.on('landing', async (data) => {
+          const { content, action, domain } = data;
+          expect(domain).toBe('link');
+          expect(action).toBe('create');
+          expect(content?.description).toBe(description);
+          expect(content?.id).toBeDefined();
+          expect(content?.url).toBe(url);
+          expect(content?.description).toBe(description);
+          socket.off('landing');
+          resolve();
+        });
+      });
+    };
+  });
+});
