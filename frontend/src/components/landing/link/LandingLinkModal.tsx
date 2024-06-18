6a3dad7158af1309bc8a658cac785cf6b654600a
surinkwon <tnfls7070@gmail.com>
2024-06-18 11:43:58 +0900

    feat: 외부 링크 유효성 검사 로직 변경
    확인 버튼을 누르면 한 번에 유효하지 않은 이유를 alert하던 것을 사용자 편의성을 위해 입력할 때마다 피드백을 주는 방식으로 변경

--- frontend/src/components/landing/link/LandingLinkModal.tsx
+++ frontend/src/components/landing/link/LandingLinkModal.tsx
@@ -7,6 +7,13 @@ interface LandingLinkModalProps {
   emitLinkCreateEvent: (content: { url: string; description: string }) => void;
 }
 
+interface ValidDataState {
+  urlValid: boolean | null;
+  urlValidText: string;
+  descriptionValid: boolean | null;
+  descriptionValidText: string;
+}
+
 const LandingLinkModal = ({
   close,
   emitLinkCreateEvent,
@@ -15,35 +22,76 @@ const LandingLinkModal = ({
     url: "",
     description: "",
   });
+  const [validData, setValidData] = useState<ValidDataState>({
+    urlValid: null,
+    urlValidText: "",
+    descriptionValid: null,
+    descriptionValidText: "",
+  });
 
-  const handleInputChange = (
-    { target }: ChangeEvent<HTMLInputElement>,
-    field: "url" | "description"
-  ) => {
+  const checkUrlValidation = (url: string): [boolean, string] => {
+    if (url.trim().length === 0) {
+      return [false, "링크 주소를 입력해주세요."];
+    }
+
+    if (decodeURI(addSchemeToURL(url)).length > 255) {
+      return [false, "링크 주소의 길이가 너무 깁니다."];
+    }
+
+    if (!isValidURL(decodeURI(addSchemeToURL(url)))) {
+      return [false, "유효하지 않은 링크 주소입니다."];
+    }
+
+    return [true, ""];
+  };
+
+  const checkDescriptionValidation = (
+    description: string
+  ): [boolean, string] => {
+    if (description.length > 255) {
+      return [false, "링크 이름의 길이가 너무 깁니다."];
+    }
+
+    if (description.trim().length === 0) {
+      return [false, "링크 이름을 입력해주세요."];
+    }
+
+    return [true, ""];
+  };
+
+  const handleUrlChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
     const { value } = target;
+    const [urlValid, urlValidText] = checkUrlValidation(value);
 
-    setLinkData({ ...linkData, [field]: decodeURI(value) });
+    setLinkData({ ...linkData, url: decodeURI(value) });
+    setValidData({ ...validData, urlValid, urlValidText });
+  };
+
+  const handleDescriptionChange = ({
+    target,
+  }: ChangeEvent<HTMLInputElement>) => {
+    const { value } = target;
+    const [descriptionValid, descriptionValidText] =
+      checkDescriptionValidation(value);
+
+    setLinkData({ ...linkData, description: value });
+    setValidData({ ...validData, descriptionValid, descriptionValidText });
   };
 
   const handleConfirmClick = () => {
     const { url, description } = linkData;
-    const errorTextList = [];
-    const urlWithScheme = addSchemeToURL(url);
+    const urlWithScheme = decodeURI(addSchemeToURL(url));
+    const [urlValid, urlValidText] = checkUrlValidation(urlWithScheme);
+    const [descriptionValid, descriptionValidText] =
+      checkDescriptionValidation(description);
 
-    if (!isValidURL(urlWithScheme)) {
-      errorTextList.push("유효하지 않은 URL입니다.");
-    }
-
-    if (urlWithScheme.trim().length > 255 || description.trim().length > 255) {
-      errorTextList.push("링크 주소와 이름은 255자 이하여야 합니다.");
-    }
-
-    if (url.trim() === "" || description.trim() === "") {
-      errorTextList.push("링크 주소와 이름은 모두 입력되어야 합니다.");
-    }
-
-    if (errorTextList.length) {
-      alert(errorTextList.join("\n"));
+    if (!urlValid || !descriptionValid) {
+      setValidData({
+        urlValid,
+        urlValidText,
+        descriptionValid,
+        descriptionValidText,
+      });
       return;
     }
 
@@ -67,21 +115,31 @@ const LandingLinkModal = ({
     >
       <div className="w-[40.625rem] bg-white flex flex-col p-9 gap-9 rounded-lg shadow-box">
         <p className="font-bold text-m">| 링크 추가하기</p>
-        <div className="flex items-center gap-8">
+        <div className="flex gap-8">
           <p className="text-xs shrink-0">링크 주소</p>
-          <input
-            className="w-full px-2 py-1 text-xs border rounded-lg border-text-gray"
-            value={linkData.url}
-            onChange={(event) => handleInputChange(event, "url")}
-          />
+          <div className="w-full">
+            <input
+              className="w-full px-2 py-1 text-xs border rounded-lg border-text-gray"
+              value={linkData.url}
+              onChange={handleUrlChange}
+            />
+            <p className={`text-xxxs text-error-red h-[1.125rem]`}>
+              {validData.urlValidText}
+            </p>
+          </div>
         </div>
-        <div className="flex items-center gap-8">
+        <div className="flex gap-8">
           <p className="text-xs shrink-0">링크 이름</p>
-          <input
-            className="w-full px-2 py-1 text-xs border rounded-lg border-text-gray"
-            value={linkData.description}
-            onChange={(event) => handleInputChange(event, "description")}
-          />
+          <div className="w-full">
+            <input
+              className="w-full px-2 py-1 text-xs border rounded-lg border-text-gray"
+              value={linkData.description}
+              onChange={handleDescriptionChange}
+            />
+            <p className="text-xxxs text-error-red h-[1.125rem]">
+              {validData.descriptionValidText}
+            </p>
+          </div>
         </div>
         <div className="flex justify-end gap-3">
           <button
