3fc6345202cf3b4fb083545c076bb18cdad0e563
조영우 <choyoungwoo9@naver.com>
2024-07-03 10:45:58 +0900

    test: 에픽 생성, 삭제, 업데이트 E2E 테스트 작성

--- backend/test/project/ws-common.ts
+++ backend/test/project/ws-common.ts
@@ -1,4 +1,61 @@
 import { Socket } from 'socket.io-client';
+import {
+  app,
+  connectServer,
+  createMember,
+  createProject,
+  getProjectLinkId,
+  joinProject,
+  memberFixture,
+  memberFixture2,
+  projectPayload,
+} from 'test/setup';
+
+export const getMemberJoinedLandingPage = async () => {
+  let socket;
+  await new Promise<void>(async (resolve, reject) => {
+    const accessToken = (await createMember(memberFixture, app)).accessToken;
+    const project = await createProject(accessToken, projectPayload, app);
+
+    socket = connectServer(project.id, accessToken);
+    handleConnectErrorWithReject(socket, reject);
+    handleErrorWithReject(socket, reject);
+    await emitJoinLanding(socket);
+    await initLanding(socket);
+    resolve();
+  });
+  return socket;
+};
+
+export const getTwoMemberJoinedLandingPage = async () => {
+  let socket1;
+  let socket2;
+  await new Promise<void>(async (resolve, reject) => {
+    // 회원1 회원가입 + 프로젝트 생성
+    const accessToken = (await createMember(memberFixture, app)).accessToken;
+    const project = await createProject(accessToken, projectPayload, app);
+    const projectLinkId = await getProjectLinkId(accessToken, project.id);
+
+    // 회원2 회원가입 + 프로젝트 참여
+    const accessToken2 = (await createMember(memberFixture2, app)).accessToken;
+    await joinProject(accessToken2, projectLinkId);
+
+    socket1 = connectServer(project.id, accessToken);
+    handleConnectErrorWithReject(socket1, reject);
+    handleErrorWithReject(socket1, reject);
+    await emitJoinLanding(socket1);
+    await initLanding(socket1);
+
+    socket2 = connectServer(project.id, accessToken2);
+    handleConnectErrorWithReject(socket2, reject);
+    handleErrorWithReject(socket2, reject);
+    await emitJoinLanding(socket2);
+    const memberId2 = await initLandingAndReturnId(socket2);
+    await expectUpdatedMemberStatus(socket1, 'on', memberId2);
+    resolve();
+  });
+  return [socket1, socket2];
+};
 
 export const handleConnectErrorWithReject = (
   socket: Socket,
