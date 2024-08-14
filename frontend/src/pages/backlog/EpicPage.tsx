0b500fdff149183fc8b9dcc742d3b267c48a46c3
surinkwon <tnfls7070@gmail.com>
2024-08-14 18:05:08 +0900

    fix: 에픽 페이지 스토리, 태스크 보이는 로직 수정

--- frontend/src/pages/backlog/EpicPage.tsx
+++ frontend/src/pages/backlog/EpicPage.tsx
@@ -1,16 +1,25 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { useOutletContext } from "react-router-dom";
 import { BacklogDTO } from "../../types/DTO/backlogDTO";
 import StoryCreateButton from "../../components/backlog/StoryCreateButton";
 import StoryCreateForm from "../../components/backlog/StoryCreateForm";
 import StoryBlock from "../../components/backlog/StoryBlock";
 import TaskBlock from "../../components/backlog/TaskBlock";
-import useShowDetail from "../../hooks/pages/backlog/useShowDetail";
 import EpicBlock from "../../components/backlog/EpicBlock";
+import TaskContainer from "../../components/backlog/TaskContainer";
+import TaskHeader from "../../components/backlog/TaskHeader";
 
 const EpicPage = () => {
   const { backlog }: { backlog: BacklogDTO } = useOutletContext();
-  const { showDetail, handleShowDetail } = useShowDetail();
+  const [showTaskList, setShowTaskList] = useState(
+    backlog.epicList.map(({ storyList }) => storyList.map(() => false))
+  );
+  const [showStory, setShowStory] = useState(
+    Array.from(new Array(backlog.epicList.length), () => ({
+      showStoryList: false,
+      showStoryForm: false,
+    }))
+  );
   const epicCategoryList = useMemo(
     () =>
       backlog.epicList.map(({ id, name, color, rankValue }) => ({
@@ -22,50 +31,105 @@ const EpicPage = () => {
     [backlog.epicList]
   );
 
-  return (
-    <div className="flex flex-col gap-4">
-      {...backlog.epicList.map(
-        ({ id: epicId, name, color, rankValue, storyList }) => (
-          <EpicBlock
-            storyExist={storyList.length > 0}
-            epic={{ id: epicId, name, color, rankValue }}
-          >
-            {...storyList.map(({ id, title, point, status, taskList }) => {
-              const progress = taskList.length
-                ? Math.round(
-                    (taskList.filter(({ status }) => status === "완료").length /
-                      taskList.length) *
-                      100
-                  )
-                : 0;
+  const handleShowTaskList = (epicIndex: number, storyIndex: number) => {
+    const newShowTaskList = showTaskList.map((innerArray1) => [...innerArray1]);
+    newShowTaskList[epicIndex][storyIndex] =
+      !newShowTaskList[epicIndex][storyIndex];
 
-              return (
-                <>
-                  <StoryBlock
-                    {...{ id, title, point, status }}
+    setShowTaskList(newShowTaskList);
+  };
+
+  const handleShowStoryList = (epicIndex: number) => {
+    const newShowStory = [...showStory];
+    const currentShowStory = newShowStory[epicIndex];
+
+    if (currentShowStory.showStoryList) {
+      newShowStory[epicIndex] = { showStoryList: false, showStoryForm: false };
+    } else {
+      newShowStory[epicIndex] = {
+        ...newShowStory[epicIndex],
+        showStoryList: true,
+      };
+    }
+
+    setShowStory(newShowStory);
+  };
+
+  const handleShowStoryForm = (epicIndex: number) => {
+    const newShowStory = [...showStory];
+    newShowStory[epicIndex] = {
+      ...newShowStory[epicIndex],
+      showStoryForm: !newShowStory[epicIndex].showStoryForm,
+    };
+
+    setShowStory(newShowStory);
+  };
+
+  return (
+    <div className="flex flex-col gap-4 pb-3">
+      {...backlog.epicList.map(
+        ({ id: epicId, name, color, rankValue, storyList }, epicIndex) => (
+          <div className="py-2 border-t border-b">
+            <EpicBlock
+              storyExist={storyList.length > 0}
+              epic={{ id: epicId, name, color, rankValue }}
+              showStoryList={showStory[epicIndex].showStoryList}
+              onShowStoryList={() => handleShowStoryList(epicIndex)}
+            />
+            {showStory[epicIndex].showStoryList && (
+              <div className="w-[65rem] ml-auto">
+                {...storyList.map(
+                  ({ id, title, point, status, taskList }, storyIndex) => {
+                    const progress = taskList.length
+                      ? Math.round(
+                          (taskList.filter(({ status }) => status === "완료")
+                            .length /
+                            taskList.length) *
+                            100
+                        )
+                      : 0;
+
+                    return (
+                      <>
+                        <StoryBlock
+                          {...{ id, title, point, status }}
+                          epic={{ id: epicId, name, color, rankValue }}
+                          progress={progress}
+                          taskExist={taskList.length > 0}
+                          showTaskList={showTaskList[epicIndex][storyIndex]}
+                          onShowTaskList={() =>
+                            handleShowTaskList(epicIndex, storyIndex)
+                          }
+                        />
+                        {showTaskList[epicIndex][storyIndex] && (
+                          <TaskContainer>
+                            <TaskHeader />
+                            {...taskList.map((task) => <TaskBlock {...task} />)}
+                          </TaskContainer>
+                        )}
+                      </>
+                    );
+                  }
+                )}
+                {showStory[epicIndex].showStoryForm ? (
+                  <StoryCreateForm
+                    epicList={epicCategoryList}
                     epic={{ id: epicId, name, color, rankValue }}
-                    progress={progress}
-                    taskExist={taskList.length > 0}
+                    onCloseClick={() => handleShowStoryForm(epicIndex)}
+                    lastStoryRankValue={
+                      storyList.length
+                        ? storyList[storyList.length - 1].rankValue
+                        : undefined
+                    }
                   />
-                  {...taskList.map((task) => <TaskBlock {...task} />)}
-                </>
-              );
-            })}
-            {showDetail ? (
-              <StoryCreateForm
-                epicList={epicCategoryList}
-                epic={{ id: epicId, name, color, rankValue }}
-                onCloseClick={() => handleShowDetail(false)}
-                lastStoryRankValue={
-                  storyList.length
-                    ? storyList[storyList.length - 1].rankValue
-                    : undefined
-                }
-              />
-            ) : (
-              <StoryCreateButton onClick={() => handleShowDetail(true)} />
+                ) : (
+                  <StoryCreateButton
+                    onClick={() => handleShowStoryForm(epicIndex)}
+                  />
+                )}
+              </div>
             )}
-          </EpicBlock>
+          </div>
         )
       )}
     </div>
--- frontend/src/pages/backlog/FinishedStoryPage.tsx
+++ frontend/src/pages/backlog/FinishedStoryPage.tsx
@@ -1,9 +1,11 @@
 import { useOutletContext } from "react-router-dom";
 import { BacklogDTO } from "../../types/DTO/backlogDTO";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import StoryBlock from "../../components/backlog/StoryBlock";
 import changeEpicListToStoryList from "../../utils/changeEpicListToStoryList";
 import TaskBlock from "../../components/backlog/TaskBlock";
+import TaskContainer from "../../components/backlog/TaskContainer";
+import TaskHeader from "../../components/backlog/TaskHeader";
 
 const FinishedStoryPage = () => {
   const { backlog }: { backlog: BacklogDTO } = useOutletContext();
@@ -25,31 +27,50 @@ const FinishedStoryPage = () => {
     [backlog.epicList]
   );
 
+  const [showTaskList, setShowTaskList] = useState(
+    new Array(storyList.length).fill(false)
+  );
+
+  const handleShowTaskList = (index: number) => {
+    const newShowTaskList = [...showTaskList];
+    newShowTaskList[index] = !newShowTaskList[index];
+    setShowTaskList(newShowTaskList);
+  };
+
   return (
     <div className="flex flex-col items-center gap-4">
       <div className="w-full border-b">
-        {...storyList.map(({ id, epic, title, point, status, taskList }) => {
-          const progress = taskList.length
-            ? Math.round(
-                (taskList.filter(({ status }) => status === "완료").length /
-                  taskList.length) *
-                  100
-              )
-            : 0;
+        {...storyList.map(
+          ({ id, epic, title, point, status, taskList }, index) => {
+            const progress = taskList.length
+              ? Math.round(
+                  (taskList.filter(({ status }) => status === "완료").length /
+                    taskList.length) *
+                    100
+                )
+              : 0;
 
-          return (
-            <>
-              <StoryBlock
-                {...{ id, title, point, status }}
-                epic={epic}
-                progress={progress}
-                taskExist={taskList.length > 0}
-                epicList={epicCategoryList}
-              />
-              {...taskList.map((task) => <TaskBlock {...task} />)}
-            </>
-          );
-        })}
+            return (
+              <>
+                <StoryBlock
+                  {...{ id, title, point, status }}
+                  epic={epic}
+                  progress={progress}
+                  taskExist={taskList.length > 0}
+                  epicList={epicCategoryList}
+                  showTaskList={showTaskList[index]}
+                  onShowTaskList={() => handleShowTaskList(index)}
+                />
+                {showTaskList[index] && (
+                  <TaskContainer>
+                    <TaskHeader />
+                    {...taskList.map((task) => <TaskBlock {...task} />)}
+                  </TaskContainer>
+                )}
+              </>
+            );
+          }
+        )}
       </div>
     </div>
   );
