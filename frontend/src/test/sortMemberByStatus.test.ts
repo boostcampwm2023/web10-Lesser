b96ef8179a4b7dadad322d130461239b2732adbe
surinkwon <tnfls7070@gmail.com>
2024-09-19 00:15:52 +0900

    test: 테스트 코드에 멤버 역할 추가

--- frontend/src/test/sortMemberByStatus.test.ts
+++ frontend/src/test/sortMemberByStatus.test.ts
@@ -4,12 +4,12 @@ import sortMemberByStatus from "../utils/sortMemberByStatus";
 describe("sortMemberByStatus test", () => {
   it("on, away, off 순 정렬 테스트", () => {
     const memberList: LandingMemberDTO[] = [
-      { id: 1, username: "", imageUrl: "", status: "off" },
-      { id: 2, username: "", imageUrl: "", status: "on" },
-      { id: 3, username: "", imageUrl: "", status: "away" },
-      { id: 4, username: "", imageUrl: "", status: "on" },
-      { id: 5, username: "", imageUrl: "", status: "off" },
-      { id: 6, username: "", imageUrl: "", status: "away" },
+      { id: 1, username: "", imageUrl: "", status: "off", role: "LEADER" },
+      { id: 2, username: "", imageUrl: "", status: "on", role: "MEMBER" },
+      { id: 3, username: "", imageUrl: "", status: "away", role: "MEMBER" },
+      { id: 4, username: "", imageUrl: "", status: "on", role: "MEMBER" },
+      { id: 5, username: "", imageUrl: "", status: "off", role: "MEMBER" },
+      { id: 6, username: "", imageUrl: "", status: "away", role: "MEMBER" },
     ];
 
     const sortedMemberIdList = memberList
